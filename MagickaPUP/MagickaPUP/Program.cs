2ad045c5d80a9c1e6bcae5071378fce97d1f2c29
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-09-29 00:47:29 +0200

    Added basic implementation for cmd args parsing
    The program now has a basic structured used to implement a basic command line argument parsing system.
    
    A new class has been created to store the data of all of the existing arguments, each with a function pointer to the function that they use under the hood.
    
    WIP : Implement file packing and unpacking, as well as path packing and unpacking.

--- MagickaPUP/MagickaPUP/Program.cs
+++ MagickaPUP/MagickaPUP/Program.cs
@@ -19,132 +19,203 @@ using MagickaPUP.MagickaClasses.Areas;
 
 namespace MagickaPUP
 {
+    public class PupProgram
+    {
+        #region Structs and Classes
+
+        public struct CmdEntry
+        {
+            public string cmd1;
+            public string cmd2;
+            public string desc1;
+            public string desc2;
+            public int args;
+            public Func<string[], int, bool> fn;
+
+            public CmdEntry(string cmd1, string cmd2, string desc1, string desc2, int args, Func<string[], int, bool> fn)
+            {
+                this.cmd1 = cmd1;
+                this.cmd2 = cmd2;
+                this.desc1 = desc1;
+                this.desc2 = desc2;
+                this.args = args;
+                this.fn = fn;
+            }
+        }
+
+        #endregion
+
+        #region Variables
+
+        private List<Packer> packers;
+        private List<Unpacker> unpackers;
+        private int debugLevel;
+
+        private CmdEntry[] commands;
+
+        #endregion
+
+        #region Constructor
+
+        public PupProgram()
+        {
+            this.packers = new List<Packer>();
+            this.unpackers = new List<Unpacker>();
+            this.debugLevel = 2; // lvl 2 by default.
+
+            this.commands = new CmdEntry[] {
+                new CmdEntry("-h", "--help", "", "Display the help message", 0, CmdHelp),
+                new CmdEntry("-d", "--debug", "<lvl>", "Set the debug logging level (default = 2)", 1, CmdDebug),
+                new CmdEntry("-p", "--pack", "<input> <output>", "Pack JSON files into XNB files", 2, CmdPack),
+                new CmdEntry("-u", "--unpack", "<input> <output>", "Unpack XNB files into JSON files", 2, CmdUnpack)
+            };
+
+            // NOTE : The pack and unpack cmd functions internally check if the input strings correspond to a file or to a directory.
+            // If it corresponds to a file, they process said file only.
+            // If it corresponds to a directory, it recursively searches for all of the files and generates in the output folder the same folder structure with the processed files.
+        }
+
+        #endregion
+
+        #region PublicMethods
+
+        public void Run(string[] args)
+        {
+            bool success = TryParseCommands(args);
+            if (!success)
+            {
+                // putln("Failed to parse program arguments.");
+                return;
+            }
+
+            foreach (var packer in this.packers)
+                packer.Pack();
+
+            foreach (var unpacker in this.unpackers)
+                unpacker.Unpack();
+        }
+
+        #endregion
+
+        #region PrivateMethods - Print
+
+        private void putln(string s = "", int indent = 0, string indentString = "  ")
+        {
+            for (int i = 0; i < indent; ++i)
+                Console.Write($"{indentString}");
+            Console.WriteLine(s);
+        }
+
+        #endregion
+
+        #region PrivateMethods - Cmd
+
+        private bool CmdHelp(string[] args, int current)
+        {
+            putln($"Usage : MagickaPup.exe --op <input file> <output file>");
+            putln();
+            putln("Commands:");
+            
+            foreach(var command in this.commands)
+                putln($"{command.cmd1}, {command.cmd2} {command.desc1} : {command.desc2}", 1);
+
+            putln();
+
+            return true;
+        }
+
+        private bool CmdPack(string[] args, int current)
+        {
+
+            return true;
+        }
+
+        private bool CmdUnpack(string[] args, int current)
+        {
+
+            return true;
+        }
+
+        private void CmdPackFile(string iFilename, string oFilename, int debuglvl = 2)
+        {
+            putln($"Registered Packer : (\"{iFilename}\", \"{oFilename}\")");
+            Packer p = new Packer(iFilename, oFilename, debuglvl);
+            this.packers.Add(p);
+        }
+
+        private void CmdUnpackFile(string iFilename, string oFilename, int debuglvl = 2)
+        {
+            putln($"Registered Unpacker : (\"{iFilename}\", \"{oFilename}\")");
+            Unpacker u = new Unpacker(iFilename, oFilename, debuglvl);
+            this.unpackers.Add(u);
+        }
+
+        private void CmdPackPath(string iPath, string oPath)
+        {
+            throw new NotImplementedException("Pack Path is not implemented yet!");
+        }
+
+        private void CmdUnpackPath(string iPath, string oPath)
+        {
+            throw new NotImplementedException("Unpack Path is not implemented yet!");
+        }
+
+        private bool TryRunCommand(string[] args, int current)
+        {
+            string arg = args[current];
+            foreach (var cmd in this.commands)
+            {
+                if (cmd.cmd1 == arg || cmd.cmd2 == arg)
+                {
+                    bool success = HasEnoughArgs(args.Length, current, cmd.args) ? cmd.fn(args, current) : false;
+                    return success;
+                }
+            }
+            putln($"Unknown or Unexpected argument detected : \"{arg}\"");
+            return false;
+        }
+
+        private bool TryParseCommands(string[] args)
+        {
+            if (args.Length <= 0)
+            {
+                CmdHelp(args, 0);
+                return false;
+            }
+
+            for (int i = 0; i < args.Length; ++i)
+            {
+                bool success = TryRunCommand(args, i);
+                if (!success)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool CmdDebug(string[] args, int current)
+        {
+            int lvl = int.Parse(args[current + 1]);
+            this.debugLevel = lvl;
+            return true;
+        }
+
+        private bool HasEnoughArgs(int argc, int current, int requiredArgs)
+        {
+            return (current + 1) + requiredArgs <= argc;
+        }
+
+        #endregion
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
-
-            /*
-            VertexBuffer buffer = new VertexBuffer();
-            
-            Thing t1 = new Thing();
-            Thing t2 = new Thing();
-            Thing t3 = new Thing();
-            t1.Value = 10;
-            t1.next = t2;
-            t2.Value = 20;
-            t2.next = t3;
-            t3.Value = 30;
-            t3.next = null;
-
-            var options = new JsonSerializerOptions
-            {
-                WriteIndented = true
-            };
-
-            string jstring = JsonSerializer.Serialize(t1, options);
-            Console.WriteLine(jstring);
-
-            Thing t = JsonSerializer.Deserialize<Thing>(jstring);
-            t.Print();
-            */
-
-            if (false)
-            {
-                IndexBuffer buf = new IndexBuffer();
-                buf.numBytes = 10;
-                //buf.data = new byte[10];
-                buf.data[0] = 255;
-                buf.data[1] = 69;
-                buf.data[5] = 34;
-                string str = JsonSerializer.Serialize(buf, new JsonSerializerOptions() { WriteIndented = true });
-                Console.WriteLine(str);
-                IndexBuffer buf2 = JsonSerializer.Deserialize<IndexBuffer>(str);
-                string str2 = JsonSerializer.Serialize(buf2, new JsonSerializerOptions() { WriteIndented = true });
-                Console.WriteLine(str2);
-            }
-
-            if (false)
-            {
-                StreamReader stream = new StreamReader("C:\\Visual Studio Projects\\MagickaPUP\\MagickaPUP\\z_test_files\\grass_woot_0.json");
-                string str = stream.ReadToEnd();
-                Texture2D tex = JsonSerializer.Deserialize<Texture2D>(str);
-
-                string outFilename = "C:\\Visual Studio Projects\\MagickaPUP\\MagickaPUP\\z_test_files\\grass_woot_0.dds";
-                var writeFile = new FileStream(outFilename, FileMode.Create, FileAccess.Write);
-                var writer = new BinaryWriter(writeFile);
-
-                writer.Write(542327876);
-
-            }
-
-            if (false) // MAIN TESTING
-            {
-                string path = "C:\\Visual Studio Projects\\MagickaPUP\\MagickaPUP\\z_test_files\\";
-                // string iFilename = "zzz_ch_swamp.xnb";
-                // string oFilename = "zzz_ch_swamp.json";
-                // string iFilename = "vs_watchtower.xnb";
-                // string oFilename = "vs_watchtower.json";
-
-
-                // string iFilename = "wc_s1.xnb";
-                // string oFilename = "wc_s1.json";
-                // string iFilename = "vs_boat.xnb";
-                // string oFilename = "vs_boat.json";
-
-                // string iFilename = "ru_s5.xnb";
-                // string oFilename = "ru_s5.json";
-                //string iFilename = "pages.xnb";
-                //string oFilename = "pages.json";
-
-                string iFilename = "tr_s8.xnb";
-                string oFilename = iFilename + ".json";
-
-                Unpacker u = new Unpacker(path + iFilename, path + oFilename, 2);
-                u.Unpack();
-            }
-
-            if (false) // MAIN TESTING 2
-            {
-                string path = "C:\\Visual Studio Projects\\MagickaPUP\\MagickaPUP\\z_test_files\\";
-                //string iFilename = "zzz_ch_swamp.json";
-                //string oFilename = "zzz_ch_swamp2.xnb";
-                //string iFilename = "wc_s1.json";
-                //string oFilename = "wc_s1_2.xnb";
-                string iFilename = "vs_boat.json";
-                string oFilename = "vs_boat_2.xnb";
-                Packer p = new Packer(path + iFilename, path + oFilename, 2);
-                p.Pack();
-            }
-
-            if (false)
-            {
-                string path = "C:\\Visual Studio Projects\\MagickaPUP\\MagickaPUP\\z_test_files\\";
-                string iFilename = "zzzzz.out";
-                string oFilename = "zzzzz_test2.txt";
-                Unpacker u = new Unpacker(path + iFilename, path + oFilename, 2);
-                u.Unpack();
-            }
-
-            // FINAL TEST!!!
-            if (true)
-            {
-                string path = "C:\\Visual Studio Projects\\MagickaPUP\\MagickaPUP\\z_test_files\\";
-                string iFilename = "untitled.json";
-                string oFilename = "zzz_untitled.xnb";
-                Packer p = new Packer(path + iFilename, path + oFilename, 2);
-                p.Pack();
-            }
-
-            if (false)
-            {
-                string path = "C:\\Visual Studio Projects\\MagickaPUP\\MagickaPUP\\z_test_files\\";
-                string iFilename = "zzz_untitled.xnb";
-                string oFilename = "zzz_untitled.json";
-                Unpacker u = new Unpacker(path + iFilename, path + oFilename, 2);
-                u.Unpack();
-            }
+            PupProgram p = new PupProgram();
+            p.Run(args);
         }
     }
 }

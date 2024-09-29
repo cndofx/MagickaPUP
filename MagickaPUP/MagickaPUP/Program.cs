c77e3b39de2b7be1a077df8bc0d82af864967552
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2024-09-29 03:40:03 +0200

    Removed unsused dependencies on main
    Removed all of the dependencies of old code, as well as the default imports added when creating a new .NET project on VS.

--- MagickaPUP/MagickaPUP/Program.cs
+++ MagickaPUP/MagickaPUP/Program.cs
@@ -1,18 +1,6 @@
-﻿using MagickaPUP.MagickaClasses;
-using MagickaPUP.MagickaClasses.Map;
-using MagickaPUP.MagickaClasses.Generic;
-using MagickaPUP.IO;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Text.Json;
-using MagickaPUP.XnaClasses;
-using MagickaPUP.MagickaClasses.Areas;
 
 // Fucking Magicka Packer-Unpacker baby!!!
 // Main Program entry point

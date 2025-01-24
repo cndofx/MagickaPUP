6c32351cd2b60f30be620c663c3f75a55e519c02
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-24 19:33:36 +0100

    Added BloodType enum

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Data/BloodType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Data
+{
+    public enum BloodType : int // mark it as int since we have to read it as an i32
+    {
+        Regular,
+        Green,
+        Black,
+        Wood,
+        Insect,
+        None,
+        COUNT // custom addition, not present in the base game's code
+    }
+}

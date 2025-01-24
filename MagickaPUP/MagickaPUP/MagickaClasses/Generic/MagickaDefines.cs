fa04a6192dbc32f70cac0b268b25579190eb19e0
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-24 22:49:35 +0100

    Created MagickaDefines static class
    The MagickaDefines class has the purpose of containing global data and methods defined specifically for Magicka related operations.
    
    The implemented methods are extracted from Magicka's code and they are the ones used internally to calculate the element indices.

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Generic/MagickaDefines.cs
@@ -0,0 +1,41 @@
+﻿using MagickaPUP.MagickaClasses.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Generic
+{
+    public static class MagickaDefines
+    {
+        #region Constants
+
+        public static readonly double ONE_OVER_LN2_F64 = 1.0d / Math.Log(2.0d);
+        public static readonly float ONE_OVER_LN2_F32 = 1.0f / (float)Math.Log(2.0f);
+
+        #endregion
+
+        #region PublicMethods
+
+        public static int ElementIndex(Elements iElement)
+        {
+            if (iElement == Elements.All)
+            {
+                return 11;
+            }
+            return (int)(Math.Log((double)iElement) * MagickaDefines.ONE_OVER_LN2_F64 + 0.5d);
+        }
+
+        public static Elements ElementFromIndex(int iIndex)
+        {
+            if (iIndex == 11)
+            {
+                return Elements.All;
+            }
+            return (Elements)(Math.Pow(2.0d, (double)iIndex) + 0.5d);
+        }
+
+        #endregion
+    }
+}

f3437927bda5d8bc06072367e5ffbbfef65ed8cf
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-24 23:14:23 +0100

    Created ModelProperties class

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/ModelProperties.cs
@@ -0,0 +1,51 @@
+﻿using MagickaPUP.IO;
+using MagickaPUP.MagickaClasses.Generic;
+using MagickaPUP.XnaClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character
+{
+    public class ModelProperties : XnaObject
+    {
+        #region Variables
+
+        public string model { get; set; }
+        public float scale { get; set; }
+        public Vec3 tint { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public ModelProperties()
+        {
+            this.model = default;
+            this.scale = 1.0f;
+            this.tint = new Vec3(1.0f, 1.0f, 1.0f);
+        }
+
+        #endregion
+
+        #region PublicMethods
+
+        public override void ReadInstance(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading ModelProperties...");
+
+            throw new NotImplementedException("Read ModelProperties is not implemented yet!");
+        }
+
+        public override void WriteInstance(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing ModelProperties...");
+
+            throw new NotImplementedException("Write ModelProperties is not implemented yet!");
+        }
+
+        #endregion
+    }
+}

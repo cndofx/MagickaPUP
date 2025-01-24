b863e697b51f1ec8c0430ce363dfbc340ad8a351
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-24 20:16:24 +0100

    Created GibReference class
    TODO : Implement the gib reading process within this class instead

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/GibReference.cs
@@ -0,0 +1,37 @@
+﻿using MagickaPUP.XnaClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character
+{
+    public class GibReference : XnaObject
+    {
+        #region Variables
+
+        public string model { get; set; }
+        public float mass { get; set; }
+        public float scale { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public GibReference()
+        {
+            this.model = default;
+            this.mass = 1.0f;
+            this.scale = 1.0f;
+        }
+
+        #endregion
+
+        #region PublicMethods
+        #endregion
+
+        #region PrivateMethods
+        #endregion
+    }
+}

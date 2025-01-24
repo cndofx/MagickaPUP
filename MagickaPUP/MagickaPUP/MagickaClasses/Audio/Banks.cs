04485f6e4b194c2fa622f30941c901df36d877a8
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-24 20:00:12 +0100

    Created Audio Banks  enum
    This enum stores the types of audio banks that exist in the game

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Audio/Banks.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Audio
+{
+    // This banks enum refers to audio banks, as in the types of an audio.
+    [Flags]
+    public enum Banks : ushort // Curiously, this is an u16 within magicka's code but also magicka reads this value as an i32, so that means it stores within the XNB files this data wasting 32 bits that will never be used lol?
+    {
+        WaveBank = 1,
+        Music = 2,
+        Ambience = 4,
+        UI = 8,
+        Spells = 16,
+        Characters = 32,
+        Footsteps = 64,
+        Weapons = 128,
+        Misc = 256,
+        Additional = 512,
+        AdditionalMusic = 1024
+    }
+}

8cada0355c1eb437ab379521c3c1bc9ca0343b7e
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-25 22:28:22 +0100

    Created EventType enum
    The event type enum is responsible for defining all of the possible event types that exist in the game.

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Events/EventType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Events
+{
+    public enum EventType
+    {
+        Damage,
+        Splash,
+        Sound,
+        Effect,
+        Remove,
+        CameraShake,
+        Decal,
+        Blast,
+        Spawn,
+        Overkill,
+        SpawnGibs,
+        SpawnItem,
+        SpawnMagick,
+        SpawnMissile,
+        Light,
+        CastMagick,
+        DamageOwner,
+        Callback
+    }
+}

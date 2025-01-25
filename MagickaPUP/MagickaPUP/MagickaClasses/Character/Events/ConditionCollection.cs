3ee616db3e4c663bf05ee9f32b6d659a92b279b8
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-25 22:25:39 +0100

    Created ConditionCollection class
    This class is responsible for holding a collection of event lists, each with their own conditions and event types.
    
    Controls part of the NPC behaviour.
    
    Potentially belongs to an utility system (US) for combat and other actions, as well ass events like dropping items and such.

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Events/ConditionCollection.cs
@@ -0,0 +1,57 @@
+﻿using MagickaPUP.IO;
+using MagickaPUP.XnaClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Events
+{
+    public class ConditionCollection : XnaObject
+    {
+        #region Variables
+
+        public int numEvents { get; set; }
+        public EventCollection[] eventCollection { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public ConditionCollection()
+        {
+            this.numEvents = 0;
+            this.eventCollection = new EventCollection[0];
+        }
+
+        #endregion
+
+        #region PublicMethods
+
+        public override void ReadInstance(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading ConditionCollection...");
+
+            this.numEvents = reader.ReadInt32();
+            this.eventCollection = new EventCollection[this.numEvents];
+            for (int i = 0; i < this.numEvents; ++i)
+                this.eventCollection[i] = EventCollection.Read(reader, logger);
+        }
+
+        public static ConditionCollection Read(MBinaryReader reader, DebugLogger logger = null)
+        {
+            var ans = new ConditionCollection();
+            ans.ReadInstance(reader, logger);
+            return ans;
+        }
+
+        public override void WriteInstance(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing ConditionCollection...");
+            throw new NotImplementedException("Write ConditionCollection not implemented yet!");
+        }
+
+        #endregion
+    }
+}

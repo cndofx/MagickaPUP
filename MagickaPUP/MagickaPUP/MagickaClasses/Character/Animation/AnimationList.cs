cb2621dc5350b6bf997c6af7707b329342a9d1b1
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-25 08:29:40 +0100

    Added ReadInstance and WriteInstance as well as constructor to AnimationList class

--- MagickaPUP/MagickaPUP/MagickaClasses/Character/Animation/AnimationList.cs
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Animation/AnimationList.cs
@@ -1,4 +1,5 @@
-﻿using MagickaPUP.XnaClasses;
+﻿using MagickaPUP.IO;
+using MagickaPUP.XnaClasses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,13 +12,41 @@ namespace MagickaPUP.MagickaClasses.Character.Animation
     // This may be a very shitty idea tho, but whatever, fuck it.
     public class AnimationList : XnaObject
     {
+        #region Constants
+
+        private static readonly int MAX_ANIMATIONS = 231; // The max number of animation clips that a CharacterTemplate's AnimationList can hold.
+
+        #endregion
+
         #region Variables
+
+        public AnimationActionClip[] animationClips { get; set; }
+
         #endregion
 
         #region Constructor
+
+        public AnimationList()
+        {
+            this.animationClips = new AnimationActionClip[MAX_ANIMATIONS];
+        }
+
         #endregion
 
         #region PublicMethods
+
+        public override void ReadInstance(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading AnimationList...");
+            throw new NotImplementedException("Read AnimationList is not implemented yet!");
+        }
+
+        public override void WriteInstance(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing AnimationList...");
+            throw new NotImplementedException("Write AnimationList is not implemented yet!");
+        }
+
         #endregion
     }
 }

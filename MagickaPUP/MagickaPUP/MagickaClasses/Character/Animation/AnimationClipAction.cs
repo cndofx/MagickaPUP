3272827943fa5c246b0776e88927256c0c2d7160
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-25 02:41:00 +0100

    Created Animation Clip classes for Character animations

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Animation/AnimationClipAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// NOTE : The animation data classes for characters are not to be confused with the XNA ones.
+// These are implemented specifically for Magicka, while the Animation classes within the XnaClasses folder are based around reading animation data
+// for XNA's Model class.
+namespace MagickaPUP.MagickaClasses.Character.Animation
+{
+    internal class AnimationClipAction
+    {
+    }
+}

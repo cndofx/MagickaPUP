7b67e1e085964922f25487269fbff5e85e490983
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-25 17:33:37 +0100

    Implemented EquipmentAttachment class
    This class describes the attachment of certain equipement items to certain bones of the skeleton of a Character.
    
    The implementation covers both reading and writing data.

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Attachments/EquipmentAttachment.cs
@@ -0,0 +1,59 @@
+﻿using MagickaPUP.IO;
+using MagickaPUP.MagickaClasses.Generic;
+using MagickaPUP.XnaClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Attachments
+{
+    public class EquipmentAttachment : XnaObject
+    {
+        #region Variables
+
+        public int Slot { get; set; }
+        public string BoneName { get; set; }
+        public Vec3 Rotation { get; set; }
+        public string ItemName { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public EquipmentAttachment()
+        {
+            this.Slot = 0;
+            this.BoneName = default;
+            this.Rotation = new Vec3();
+            this.ItemName = default;
+        }
+
+        #endregion
+
+        #region PublicMethods
+
+        public override void ReadInstance(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading EquipmentAttachment...");
+
+            this.Slot = reader.ReadInt32();
+            this.BoneName = reader.ReadString();
+            this.Rotation = Vec3.Read(reader, null);
+            this.ItemName = reader.ReadString();
+        }
+
+        public override void WriteInstance(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing EquipmentAttachment...");
+
+            writer.Write(this.Slot);
+            writer.Write(this.BoneName);
+            this.Rotation.WriteInstance(writer, null);
+            writer.Write(this.ItemName);
+        }
+
+        #endregion
+    }
+}

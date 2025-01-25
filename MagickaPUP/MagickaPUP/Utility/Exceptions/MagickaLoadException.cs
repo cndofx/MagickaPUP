ea46d1dd2525acf5221465c88e79d599ab41b12b
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-25 05:32:16 +0100

    Created MagickaLoadException class
    The MagickaLoadException class is an Exception class that is used to handle errors when loading data that would not be valid within Magicka when loaded but that we cannot filter out through JSON alone.
    
    An example of where this is useful is when loading data for CharacterTemplates, such as defining an input JSON with a list of lights with more than 4 point lights in total. That is not valid and actually generates a runtime exception within Magicka's code, so we can do the same to prevent us from compiling XNBs that will not work in the first place.
    
    Note that this could be modified in the future and completely discarded if the engine rewrite project ends up allowing extensions that remove some of these limits... but that's a project for the far future and should not limit the correctness of this project since it does not even exist in the first place.
    
    NOTE : Similar to how version specific data structures loading will be implemented with a flag for the packer/unpacker program, we can also define a flag for the engine rewrite support in the future, so this is alright for now.

---
+++ MagickaPUP/MagickaPUP/Utility/Exceptions/MagickaLoadException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MagickaPUP.Utility.Exceptions
+{
+    /*
+        NOTE : The exception should have some kind of message, something like $"The following content cannot be loaded by Magicka: {whatever_thing_name}"
+        and then the user defined message or whatever... something like that.
+        For now, this is good enough. Heck, we could even just throw regular exceptions and call it a day, but doing this early makes things easier in the long run...
+        For example, it can help identifying when an exception happens because of something that is always invalid when loaded into Magicka, or when something is
+        valid only on some specific versions and on others it does not work, etc...
+    */
+    // Exception class used to handle errors when loading data that would not be valid within Magicka when loaded but that we cannot filter out through JSON alone.
+    public class MagickaLoadException : Exception
+    {
+        public MagickaLoadException()
+        :base()
+        { }
+
+        public MagickaLoadException(string message)
+        :base(message)
+        { }
+
+        public MagickaLoadException(string message, Exception inner)
+        :base(message, inner)
+        { }
+    }
+}

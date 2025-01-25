b75e7b681ecb6b9f77b2a737bc128bed65feaaaa
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-25 08:23:01 +0100

    Refactored AnimationActionClipBuffer into AnimationList class

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Animation/AnimationList.cs
@@ -0,0 +1,23 @@
+﻿using MagickaPUP.XnaClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Animation
+{
+    // NOTE : This class does NOT exist within Magicka's code, this is an abstraction made to make things easier for me to handle and to make the serialized JSON look nicer.
+    // This may be a very shitty idea tho, but whatever, fuck it.
+    public class AnimationList : XnaObject
+    {
+        #region Variables
+        #endregion
+
+        #region Constructor
+        #endregion
+
+        #region PublicMethods
+        #endregion
+    }
+}

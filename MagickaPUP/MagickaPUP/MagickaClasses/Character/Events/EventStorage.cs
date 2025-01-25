82a285f86f0f7d4a2930ba049ddd851fb3b7c574
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-25 22:34:02 +0100

    Created EventStorage class
    The event storage class is responsible for holding the data of a specific event in memory.
    
    It contains an event type enum and a Magicka Event property.
    
    NOTE : Read the TODOs on the previous commit to remove duplication of identifying strings and enum values when working with .NET's json serializer. Maybe the solution could be to add a static field to each specific class that returns the enum value for said type identifier, and that way we no longer hold the value within the EventStorage class itself. But if we're going to do that, we might as well modify the code so that we don't hold the length of lists on a separate variable within the JSON...

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Events/EventStorage.cs
@@ -0,0 +1,64 @@
+﻿using MagickaPUP.IO;
+using MagickaPUP.XnaClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MagickaPUP.Utility.Exceptions;
+
+namespace MagickaPUP.MagickaClasses.Character.Events
+{
+    // This class is a major pain in the fucking asshole and I will not miss having had to work on it.
+    // Whoever programmed Magicka's event system should be in jail. That is all I can say.
+    public class EventStorage : XnaObject
+    {
+        #region Variables
+
+        public EventType EventType { get; set; }
+        public MagickaEvent Event { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public EventStorage()
+        {
+            this.EventType = default;
+            this.Event = default;
+        }
+
+        #endregion
+
+        #region PublicMethods
+
+        public override void ReadInstance(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading EventStorage...");
+
+            this.EventType = (EventType)reader.ReadByte();
+            switch (this.EventType)
+            {
+                // TODO : Implement cases for each and every single type of MagickaEvent.
+                default:
+                    throw new MagickaLoadException($"Event type \"{((byte)this.EventType)}\" not recognised as a valid Magicka Event!");
+                    break;
+            }
+        }
+
+        public static EventStorage Read(MBinaryReader reader, DebugLogger logger = null)
+        {
+            var ans = new EventStorage();
+            ans.ReadInstance(reader, logger);
+            return ans;
+        }
+
+        public override void WriteInstance(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing EventStorage...");
+            throw new NotImplementedException("Write EventStorage is not implemented yet!");
+        }
+
+        #endregion
+    }
+}

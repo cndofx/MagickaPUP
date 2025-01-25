d28c20bda2a5de8aea29c6c1b12fa55cbb50cee8
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-25 22:27:17 +0100

    Added EventConditionType enum
    This enum holds all of the different types of event conditions that exist in game.

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Events/EventConditionType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Events
+{
+    [Flags]
+    public enum EventConditionType : byte
+    {
+        Default = 1,
+        Hit = 2,
+        Collision = 4,
+        Damaged = 8,
+        Timer = 16,
+        Death = 32,
+        OverKill = 64
+    }
+}

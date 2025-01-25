ba0c14e8566a7a64b3fa2855e599d7321fd95466
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-25 15:21:11 +0100

    Added basic boilerplate for reading and writing EffectHolder and SoundHolder

--- MagickaPUP/MagickaPUP/MagickaClasses/Character/Attachments/EffectHolder.cs
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Attachments/EffectHolder.cs
@@ -1,4 +1,6 @@
-﻿using MagickaPUP.XnaClasses;
+﻿using MagickaPUP.IO;
+using MagickaPUP.MagickaClasses.Audio;
+using MagickaPUP.XnaClasses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +12,43 @@ namespace MagickaPUP.MagickaClasses.Character.Attachments
     public class EffectHolder : XnaObject
     {
         #region Variables
+
+        public string BoneName { get; set; }
+        public string EffectName { get; set; }
+
         #endregion
 
         #region Constructor
+
+        public EffectHolder()
+        {
+            this.BoneName = default;
+            this.EffectName = default;
+        }
+
         #endregion
 
         #region PublicMethods
+
+        public override void ReadInstance(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading EffectHolder...");
+            throw new NotImplementedException("Read EffectHolder is not implemented yet!");
+        }
+
+        public static EffectHolder Read(MBinaryReader reader, DebugLogger logger = null)
+        {
+            var ans = new EffectHolder();
+            ans.ReadInstance(reader, logger);
+            return ans;
+        }
+
+        public override void WriteInstance(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing EffectHolder...");
+            throw new NotImplementedException("Write EffectHolder is not implemented yet!");
+        }
+
         #endregion
     }
 }
--- MagickaPUP/MagickaPUP/MagickaClasses/Character/Attachments/SoundHolder.cs
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Attachments/SoundHolder.cs
@@ -1,4 +1,6 @@
-﻿using MagickaPUP.XnaClasses;
+﻿using MagickaPUP.IO;
+using MagickaPUP.MagickaClasses.Audio;
+using MagickaPUP.XnaClasses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +12,43 @@ namespace MagickaPUP.MagickaClasses.Character.Attachments
     public class SoundHolder : XnaObject
     {
         #region Variables
+
+        public string SoundName { get; set; }
+        public Banks SoundBanks { get; set; }
+
         #endregion
 
         #region Constructor
+
+        public SoundHolder()
+        {
+            this.SoundName = default;
+            this.SoundBanks = default;
+        }
+
         #endregion
 
         #region PublicMethods
+
+        public override void ReadInstance(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading SoundHolder...");
+            throw new NotImplementedException("Read SoundHolder is not implemented yet!");
+        }
+
+        public static SoundHolder Read(MBinaryReader reader, DebugLogger logger = null)
+        {
+            var ans = new SoundHolder();
+            ans.ReadInstance(reader, logger);
+            return ans;
+        }
+
+        public override void WriteInstance(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing SoundHolder...");
+            throw new NotImplementedException("Write SoundHolder is not implemented yet!");
+        }
+
         #endregion
     }
 }

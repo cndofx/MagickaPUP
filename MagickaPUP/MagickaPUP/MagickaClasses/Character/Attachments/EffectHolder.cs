33a493e974de5245b5ada829ceed248dd8ad8e1d
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-25 14:52:10 +0100

    Created Holder classes for all attachments for CharacterTemplate
    TODO : Maybe rename to WhateverAttachment or WhateverCharacterAttachment?

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Attachments/EffectHolder.cs
@@ -0,0 +1,21 @@
+﻿using MagickaPUP.XnaClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Attachments
+{
+    public class EffectHolder : XnaObject
+    {
+        #region Variables
+        #endregion
+
+        #region Constructor
+        #endregion
+
+        #region PublicMethods
+        #endregion
+    }
+}
---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Attachments/SoundHolder.cs
@@ -0,0 +1,21 @@
+﻿using MagickaPUP.XnaClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Attachments
+{
+    public class SoundHolder : XnaObject
+    {
+        #region Variables
+        #endregion
+
+        #region Constructor
+        #endregion
+
+        #region PublicMethods
+        #endregion
+    }
+}

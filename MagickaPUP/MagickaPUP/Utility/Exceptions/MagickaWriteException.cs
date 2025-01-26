faddce4c52bb4a61d9861a821f3238a782374237
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-26 04:57:43 +0100

    Created MagickaWriteException
    Pretty much it's the same as the read exception, only that it has a default message that makes more sense for write operations.

---
+++ MagickaPUP/MagickaPUP/Utility/Exceptions/MagickaWriteException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.Utility.Exceptions
+{
+    public class MagickaWriteException : Exception
+    {
+        private static readonly string EXCEPTION_MSG_BASE = "Write Error";
+
+        public MagickaWriteException()
+        : base($"{EXCEPTION_MSG_BASE} : An error occurred while reading input data")
+        { }
+
+        public MagickaWriteException(string message)
+        : base($"{EXCEPTION_MSG_BASE} : {message}")
+        { }
+
+        public MagickaWriteException(string message, Exception inner)
+        : base($"{EXCEPTION_MSG_BASE} : {message}", inner)
+        { }
+    }
+}

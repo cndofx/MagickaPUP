e72dc1079cbc69e3c6df0e624c597766df74ae76
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-26 16:24:08 +0100

    Added AI behaviour related Order and ReactTo enums

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Data/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Data
+{
+    // This enum describes the order of an AI agent, as in, the action it is commanded to perform when a certain event takes place.
+    // SpawnEvent contains one of these to command the AI to perform a specific behaviour on spawn.
+    public enum Order : byte
+    {
+        None,
+        Idle,
+        Attack,
+        Defend,
+        Flee,
+        Wander,
+        Panic
+    }
+}
---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Data/ReactTo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Data
+{
+    // This enum contains flags to set what action an NPC is supposed to react to.
+    [Flags]
+    public enum ReactTo : byte
+    {
+        None = 0,
+        Attack = 1,
+        Proximity = 2
+    }
+}

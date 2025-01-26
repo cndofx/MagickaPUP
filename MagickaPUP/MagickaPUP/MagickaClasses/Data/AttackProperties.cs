643d7b884e5f7a212078e36e4395c44874ff2558
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-26 01:22:24 +0100

    Added AttackProperties enum

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Data/AttackProperties.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Data
+{
+    [Flags]
+    public enum AttackProperties : short
+    {
+        Damage = 1,
+        Knockdown = 2,
+        Pushed = 4,
+        Knockback = 6,
+        Piercing = 8,
+        ArmourPiercing = 16,
+        Status = 32,
+        Entanglement = 64,
+        Stun = 128,
+        Bleed = 256,
+        NumberOfTypes = 512
+    }
+}

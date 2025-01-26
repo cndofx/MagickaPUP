630cb05f085c7d1c9dc634ba11430c9130589687
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-26 01:54:50 +0100

    Implemented EventStorage switch event reading

--- MagickaPUP/MagickaPUP/MagickaClasses/Character/Events/EventStorage.cs
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Events/EventStorage.cs
@@ -39,10 +39,64 @@ namespace MagickaPUP.MagickaClasses.Character.Events
             this.EventType = (EventType)reader.ReadByte();
             switch (this.EventType)
             {
-                // TODO : Implement cases for each and every single type of MagickaEvent.
+                // NOTE : We could do this with an array and some function pointers, but we're going with the switch-case ladder that Magicka does for consistency.
+                case EventType.Damage:
+                    this.Event = DamageEvent.Read(reader, logger);
+                    break;
+                case EventType.Splash:
+                    this.Event = SplashEvent.Read(reader, logger);
+                    break;
+                case EventType.Sound:
+                    this.Event = PlaySoundEvent.Read(reader, logger);
+                    break;
+                case EventType.Effect:
+                    this.Event = PlayEffectEvent.Read(reader, logger);
+                    break;
+                case EventType.Remove:
+                    this.Event = RemoveEvent.Read(reader, logger);
+                    break;
+                case EventType.CameraShake:
+                    this.Event = CameraShakeEvent.Read(reader, logger);
+                    break;
+                case EventType.Decal:
+                    this.Event = SpawnDecalEvent.Read(reader, logger);
+                    break;
+                case EventType.Blast:
+                    this.Event = BlastEvent.Read(reader, logger);
+                    break;
+                case EventType.Spawn:
+                    this.Event = SpawnEvent.Read(reader, logger);
+                    break;
+                case EventType.Overkill:
+                    this.Event = OverKillEvent.Read(reader, logger);
+                    break;
+                case EventType.SpawnGibs:
+                    this.Event = SpawnGibsEvent.Read(reader, logger);
+                    break;
+                case EventType.SpawnItem:
+                    this.Event = SpawnItemEvent.Read(reader, logger);
+                    break;
+                case EventType.SpawnMagick:
+                    this.Event = SpawnMagickEvent.Read(reader, logger);
+                    break;
+                case EventType.SpawnMissile:
+                    this.Event = SpawnMissileEvent.Read(reader, logger);
+                    break;
+                case EventType.Light:
+                    this.Event = LightEvent.Read(reader, logger);
+                    break;
+                case EventType.CastMagick:
+                    this.Event = CastMagickEvent.Read(reader, logger);
+                    break;
+                case EventType.DamageOwner:
+                    this.Event = DamageOwnerEvent.Read(reader, logger);
+                    break;
+                case EventType.Callback:
+                    this.Event = CallbackEvent.Read(reader, logger);
+                    break;
                 default:
                     throw new MagickaLoadException($"Event type \"{((byte)this.EventType)}\" not recognised as a valid Magicka Event!");
-                    break;
+                    break; // lol
             }
         }
 

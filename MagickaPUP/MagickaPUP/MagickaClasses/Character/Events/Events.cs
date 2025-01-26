958b4887ed711c0b4cdffd030d9d47ffe028d964
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-26 04:58:02 +0100

    Implemented SplashEvent reading and writing

--- MagickaPUP/MagickaPUP/MagickaClasses/Character/Events/Events.cs
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Events/Events.cs
@@ -3,7 +3,7 @@ using MagickaPUP.XnaClasses;
 using System;
 using System.Collections.Generic;
 using MagickaPUP.MagickaClasses.Data;
-using System.Runtime.Remoting.Messaging;
+using MagickaPUP.Utility.Exceptions;
 
 namespace MagickaPUP.MagickaClasses.Character.Events
 {
@@ -31,7 +31,7 @@ namespace MagickaPUP.MagickaClasses.Character.Events
     {
         #region Variables
 
-        public AttackProperties AttackProperties { get; set; }
+        public AttackProperties AttackProperty { get; set; }
         public Elements Element { get; set; }
         public float Amount { get; set; }
         public float Magnitude { get; set; }
@@ -43,7 +43,7 @@ namespace MagickaPUP.MagickaClasses.Character.Events
 
         public DamageEvent()
         {
-            this.AttackProperties = default;
+            this.AttackProperty = default;
             this.Element = default;
             this.Amount = 0.0f;
             this.Magnitude = 0.0f;
@@ -58,7 +58,7 @@ namespace MagickaPUP.MagickaClasses.Character.Events
         {
             logger?.Log(1, "Reading DamageEvent...");
 
-            this.AttackProperties = (AttackProperties)reader.ReadInt32();
+            this.AttackProperty = (AttackProperties)reader.ReadInt32();
             this.Element = (Elements)reader.ReadInt32();
             this.Amount = reader.ReadSingle();
             this.Magnitude = reader.ReadSingle();
@@ -76,7 +76,7 @@ namespace MagickaPUP.MagickaClasses.Character.Events
         {
             logger?.Log(1, "Writing DamageEvent...");
 
-            writer.Write((int)this.AttackProperties);
+            writer.Write((int)this.AttackProperty);
             writer.Write((int)this.Element);
             writer.Write(this.Amount);
             writer.Write(this.Magnitude);
@@ -88,12 +88,74 @@ namespace MagickaPUP.MagickaClasses.Character.Events
 
     public class SplashEvent : MagickaEvent
     {
+        #region Constants
+
+        // This exception message is basically the same for both read and write operations, so we can just reuse it.
+        private static readonly string EXCEPTION_MSG = $"Magicka does not support SplashEvents with a Radius value smaller than float.Epsilon ({float.Epsilon})";
+
+        #endregion
+
+        #region Variables
+
+        public AttackProperties AttackProperty { get; set; }
+        public Elements Element { get; set; }
+        public int Amount { get; set; } // NOTE : Take a look at the read code for further context about the data type of this variable...
+        public float Magnitude { get; set; }
+        public float Radius { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public SplashEvent()
+        {
+            this.AttackProperty = default;
+            this.Element = default;
+            this.Amount = 0;
+            this.Magnitude = 0.0f;
+            this.Radius = 1.0f;
+        }
+
+        #endregion
+
+        #region PublicMethods
+
+        public override void ReadInstance(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading SplashEvent...");
+
+            this.AttackProperty = (AttackProperties)reader.ReadInt32();
+            this.Element = (Elements)reader.ReadInt32();
+            this.Amount = reader.ReadInt32(); // WTF, why does Magicka store this value as an i32 within XNB files for SplashEvents but not for DamageEvents???
+            this.Magnitude = reader.ReadSingle();
+            this.Radius = reader.ReadSingle();
+
+            if (this.Radius <= float.Epsilon) // NOTE : Magicka hardcodes the value as 1E-45f, but we can just use float.Epsilon here. This makes sense tho, we can't have a radius that is 0 or less than 0, and the smartest comparison is against the smallest possible valid value for a floating point number.
+                throw new MagickaLoadException(EXCEPTION_MSG);
+        }
+
         public static SplashEvent Read(MBinaryReader reader, DebugLogger logger = null)
         {
             var ans = new SplashEvent();
             ans.ReadInstance(reader, logger);
             return ans;
         }
+
+        public override void WriteInstance(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing SplashEvent...");
+
+            writer.Write((int)this.AttackProperty);
+            writer.Write((int)this.Element);
+            writer.Write(this.Amount);
+            writer.Write(this.Magnitude);
+            writer.Write(this.Radius);
+
+            if (this.Radius <= float.Epsilon)
+                throw new MagickaWriteException(EXCEPTION_MSG);
+        }
+
+        #endregion
     }
 
     public class PlaySoundEvent : MagickaEvent

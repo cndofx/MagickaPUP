d2a0b69c76f827dd55fa11f3a46d972f3c6d7346
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-26 17:48:31 +0100

    Added MagickType enum
    This enum contains a list of all of the magicks that exist in Magicka.

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Data/MagickType.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Data
+{
+    // This enum contains a list of all of the magicks that exist in Magicka.
+    // Yes, all of the magicks were hardcoded and there is no way to add new custom magicks without extending the executable... or is there? Hey, Vsauce! Michael here!
+    public enum MagickType
+    {
+        None,
+        Revive,
+        Grease,
+        Haste,
+        Invisibility,
+        Teleport,
+        Fear,
+        Charm,
+        ThunderB,
+        Rain,
+        Tornado,
+        Blizzard,
+        MeteorS,
+        Conflagration,
+        ThunderS,
+        TimeWarp,
+        Vortex,
+        SUndead,
+        SElemental,
+        SDeath,
+        SPhoenix,
+        Nullify,
+        Corporealize,
+        CTD,
+        Napalm,
+        Portal,
+        TractorPull,
+        ProppMagick,
+        Levitate,
+        ChainLightning,
+        Confuse,
+        Wave,
+        PerformanceEnchantment,
+        JudgementSpray,
+        Amalgameddon,
+        NrOfMagicks,
+        Grow,
+        Shrink,
+        Etherealize,
+        Polymorph,
+        Earthquake,
+        Votal,
+        KnockDownChance,
+        SummonCross,
+        VladZap,
+        SpawnSlime,
+        SpawnSlimeOverkill,
+        GreaseSplash,
+        EtherealClone,
+        InflictBleedDamage,
+        InflictPoisonDamage
+    }
+}

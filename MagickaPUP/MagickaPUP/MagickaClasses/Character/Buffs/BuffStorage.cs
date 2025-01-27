3130a23e387e55f405378d5e3abfdcb16d3c4564
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-27 23:54:20 +0100

    Created BuffStorage class

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Buffs/BuffStorage.cs
@@ -0,0 +1,25 @@
+﻿using MagickaPUP.IO;
+using MagickaPUP.MagickaClasses.Generic;
+using System;
+using System.Collections.Generic;
+using MagickaPUP.MagickaClasses.Data;
+
+namespace MagickaPUP.MagickaClasses.Character.Buffs
+{
+    public class BuffStorage
+    {
+        public BuffType BuffType { get; set; }
+        public VisualCategory VisualCategory { get; set; }
+        public Vec3 Color { get; set; }
+        public float Time { get; set; }
+        public string Effect { get; set; }
+
+        public BuffStorage()
+        { }
+
+        public BuffStorage(MBinaryReader reader, DebugLogger logger = null)
+        {
+
+        }
+    }
+}

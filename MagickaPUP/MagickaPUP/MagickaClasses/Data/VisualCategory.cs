72729b7a0441950e39010aae0b29bf14cb9e7396
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-27 23:54:07 +0100

    Added VisualCategory enum

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Data/VisualCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Data
+{
+    public enum VisualCategory : byte
+    {
+        None,
+        Offensive,
+        Defensive,
+        Special
+    }
+}

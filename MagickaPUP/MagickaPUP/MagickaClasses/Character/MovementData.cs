38b36a5ee463ad89008949988de7a693331bce4a
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-27 23:33:24 +0100

    Created MovementData class
    The MovementData class holds data related to movement properties and animations that will be used by a character as defined in the CharacterTemplate class.

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/MovementData.cs
@@ -0,0 +1,33 @@
+﻿using MagickaPUP.IO;
+using MagickaPUP.MagickaClasses.Character.Animation;
+using MagickaPUP.MagickaClasses.Data;
+using MagickaPUP.XnaClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character
+{
+    public class MovementData
+    {
+        public MovementProperties MovementProperties { get; set; }
+        public string[] Animations { get; set; }
+
+        public MovementData()
+        {
+            this.MovementProperties = default(MovementProperties);
+            this.Animations = Array.Empty<string>();
+        }
+
+        public MovementData(MBinaryReader reader, DebugLogger logger = null)
+        {
+            this.MovementProperties = (MovementProperties)reader.ReadByte();
+            this.Animations = new string[reader.ReadInt32()];
+            for (int i = 0; i < this.Animations.Length; ++i)
+                this.Animations[i] = reader.ReadString();
+        }
+
+    }
+}

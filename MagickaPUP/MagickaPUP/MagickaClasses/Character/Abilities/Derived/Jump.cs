94ec0e568c61bdf4d28443871d1cec3f506c113e
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-27 19:11:38 +0100

    Implemented Jump Ability

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Abilities/Derived/Jump.cs
@@ -0,0 +1,43 @@
+﻿using MagickaPUP.IO;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Abilities.Derived
+{
+    public class Jump : Ability
+    {
+        public float MinRange { get; set; }
+        public float MaxRange { get; set; }
+        public float Angle { get; set; }
+        public float Elevation { get; set; }
+
+        public Jump()
+        {
+            this.MinRange = 0.0f;
+            this.MaxRange = 1.0f;
+            this.Angle = 0.0f;
+            this.Elevation = 1.0f;
+        }
+
+        public override void ReadInstance(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading Jump Ability...");
+
+            this.MaxRange = reader.ReadSingle();
+            this.MinRange = reader.ReadSingle();
+            this.Angle = reader.ReadSingle();
+            this.Elevation = reader.ReadSingle();
+        }
+
+        public override void WriteInstance(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing Jump Ability...");
+            throw new NotImplementedException("Write Jump Ability is not implemented yet!");
+        }
+    }
+}

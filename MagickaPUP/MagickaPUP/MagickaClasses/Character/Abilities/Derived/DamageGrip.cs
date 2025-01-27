7d43163ec3ad27a08758b93fdcde8b8de71c6895
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-27 19:11:07 +0100

    Implemented DamageGrip Ability

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Abilities/Derived/DamageGrip.cs
@@ -0,0 +1,26 @@
+﻿using MagickaPUP.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Abilities.Derived
+{
+    // Same as ConfuseGrip. Read the comment in that class for context.
+    public class DamageGrip : Ability
+    {
+        public DamageGrip()
+        { }
+
+        public override void ReadInstance(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading DamageGrip Ability...");
+        }
+
+        public override void WriteInstance(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing DamageGrip Ability...");
+        }
+    }
+}

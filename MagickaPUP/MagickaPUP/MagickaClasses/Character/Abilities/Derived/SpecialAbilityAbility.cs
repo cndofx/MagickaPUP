b2191c2ba05c832e5e12258c6139f8ccaee5c81c
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-27 19:12:23 +0100

    Implemented SpecialAbilityAbility Ability

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Abilities/Derived/SpecialAbilityAbility.cs
@@ -0,0 +1,41 @@
+﻿using MagickaPUP.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Abilities.Derived
+{
+    public class SpecialAbilityAbility : Ability
+    {
+        public float MinRange { get; set; }
+        public float MaxRange { get; set; }
+        public float Angle { get; set; }
+        public int Weapon { get; set; }
+
+        public SpecialAbilityAbility()
+        {
+            this.MinRange = 0.0f;
+            this.MaxRange = 1.0f;
+            this.Angle = 0.0f;
+            this.Weapon = 0;
+        }
+
+        public override void ReadInstance(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading SpecialAbilityAbility...");
+
+            this.MaxRange = reader.ReadSingle();
+            this.MinRange = reader.ReadSingle();
+            this.Angle = reader.ReadSingle();
+            this.Weapon = reader.ReadInt32();
+        }
+
+        public override void WriteInstance(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing SpecialAbilityAbility...");
+            throw new NotImplementedException("Write SpecialAbilityAbility is not implemented yet!");
+        }
+    }
+}

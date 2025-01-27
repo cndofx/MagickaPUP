753e24d3e8cfd46a1666a9543facdcb054092f81
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-27 19:08:37 +0100

    Created Damage struct
    The Damage struct contains the properties / fields required to describe a type of damage and the amount of damage to be applied to an entity.
    
    TODO : Rewrite some of the old CharacterTemplate related code to use this Damage struct rather than doing its own thing.
    
    TODO : Figure out what to do when rewriting the whole XnaObject based system, because as you can see, mpup's philosophy of "everything can be a primary object within an XNB file" is turning out to be quite a pain in the ass to maintain, as most certainly, even if that were true, certain objects just have inconsistent writing and reading orders, thus those would CLEARLY NEVER be used as primary objects within a file. Even if the objective is to offer as much extensibility as possible and support for future modded versions of the engine with custom behaviours and structures, maybe some limits should be applied.

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Damage.cs
@@ -0,0 +1,46 @@
+﻿using MagickaPUP.MagickaClasses.Data;
+using MagickaPUP.XnaClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character
+{
+    // NOTE : This struct goes unused because of an obvious problem: each time Damage is read within Magicka's code, it is performed with completely different
+    // orders and data types, as the reads are performed inline, so pretty much this is useless for us, specially since sometimes damage has float fields and sometimes
+    // integer fields. Under the hood within Magicka it is all turned into floats, but if we did the same we would have some precission problems and unncessary
+    // conversions in between, so it's better to just hard code this shit for now I suppose...
+
+    // For now I'll just make a compromise on the precission loss and fuck it. Things would be easier with unions, or maybe make 2 different Damage types for mpup,
+    // but whatever. We'll just convert to float, which is what Magicka does anyway, so we can be at least consistent enough where users don't have to question their
+    // sanity...
+
+    public struct Damage
+    {
+        public AttackProperties AttackProperty { get; set; }
+        public Elements Element { get; set; }
+        public float Amount { get; set; }
+        public float Magnitude { get; set; }
+
+        public Damage(AttackProperties attackProperty = default, Elements element = default, float amount = 1.0f, float magnitude = 1.0f)
+        {
+            this.AttackProperty = attackProperty;
+            this.Element = element;
+            this.Amount = amount;
+            this.Magnitude = magnitude;
+        }
+
+        /*
+        public void Read_iiff()
+        { }
+
+        public void Read_iiif()
+        { }
+
+        public void Read_iiii()
+        { }
+        */
+    }
+}

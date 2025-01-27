7759419e091dc495918e2d6114984a71067aae8c
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-27 19:11:00 +0100

    Implemented ConfuseGrip Ability

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Abilities/Derived/ConfuseGrip.cs
@@ -0,0 +1,26 @@
+﻿using MagickaPUP.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Abilities.Derived
+{
+    // NOTE : This class literally reads fucking NOTHING in Magicka's code, so here it exists as a sort of proxy for the reading process.
+    internal class ConfuseGrip : Ability
+    {
+        public ConfuseGrip()
+        { }
+
+        public override void ReadInstance(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading ConfuseGrip Ability...");
+        }
+
+        public override void WriteInstance(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing ConfuseGrip Ability...");
+        }
+    }
+}

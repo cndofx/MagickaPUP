988337016ef2d0f39284f72eb69413d4d3c37b59
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-27 19:11:51 +0100

    Implemented PickUpCharacter Ability

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Abilities/Derived/PickUpCharacter.cs
@@ -0,0 +1,46 @@
+﻿using MagickaPUP.IO;
+using MagickaPUP.MagickaClasses.Character.Animation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Abilities.Derived
+{
+    public class PickUpCharacter : Ability
+    {
+        public float MinRange { get; set; }
+        public float MaxRange { get; set; }
+        public float Angle { get; set; }
+        public float MaxWeight { get; set; }
+        public string DropAnimation { get; set; }
+
+        public PickUpCharacter()
+        {
+            this.MinRange = 0.0f;
+            this.MaxRange = 1.0f;
+            this.Angle = 0.0f;
+            this.MaxWeight = 0.0f;
+            this.DropAnimation = default;
+        }
+
+        public override void ReadInstance(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading PickUpCharacter Ability...");
+
+            this.MaxRange = reader.ReadSingle();
+            this.MinRange = reader.ReadSingle();
+            this.Angle = reader.ReadSingle();
+            this.MaxWeight = reader.ReadSingle();
+            this.DropAnimation = reader.ReadString();
+        }
+
+        public override void WriteInstance(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing PickUpCharacterAbility...");
+            throw new NotImplementedException("Write PickUpCharacterAbility is not implemented yet!");
+        }
+
+    }
+}

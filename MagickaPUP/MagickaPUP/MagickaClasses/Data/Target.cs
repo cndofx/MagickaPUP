aa45a89114fcb59357839ba6446e5170a869855b
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-27 19:05:12 +0100

    Created Target enum

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Data/Target.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Data
+{
+    public enum Target : byte
+    {
+        Self = 1,
+        S = 1,
+        Enemy = 2,
+        E = 2,
+        Friendly = 3,
+        F = 3
+    }
+}

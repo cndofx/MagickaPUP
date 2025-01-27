db720e6f75980e5138be9cc204e261cbcd2183df
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-27 19:11:33 +0100

    Implemented GripCharacterFromBehind Ability

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Abilities/Derived/GripCharacterFromBehind.cs
@@ -0,0 +1,41 @@
+﻿using MagickaPUP.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Abilities.Derived
+{
+    public class GripCharacterFromBehind : Ability
+    {
+        public float MinRange { get; set; }
+        public float MaxRange { get; set; }
+        public float Angle { get; set; }
+        public float MaxWeight { get; set; }
+
+        public GripCharacterFromBehind()
+        {
+            this.MinRange = 0.0f;
+            this.MaxRange = 1.0f;
+            this.Angle = 0.0f;
+            this.MaxWeight = 0.0f;
+        }
+
+        public override void ReadInstance(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading GripCharacterFromBehind Ability...");
+
+            this.MaxRange = reader.ReadSingle();
+            this.MinRange = reader.ReadSingle();
+            this.Angle = reader.ReadSingle();
+            this.MaxWeight = reader.ReadSingle();
+        }
+
+        public override void WriteInstance(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing GripCharacterFromBehind Ability...");
+            throw new NotImplementedException("Write GripCharacterFromBehind is not implemented yet!");
+        }
+    }
+}

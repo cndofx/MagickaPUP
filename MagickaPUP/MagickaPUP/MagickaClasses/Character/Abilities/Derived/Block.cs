21484facb9b8f81a41dcb29cc8f637695e68f257
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-27 19:10:48 +0100

    Implemented Block Ability

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Abilities/Derived/Block.cs
@@ -0,0 +1,37 @@
+﻿using MagickaPUP.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Abilities.Derived
+{
+    public class Block : Ability
+    {
+        public float Arc { get; set; }
+        public int Shield { get; set; }
+
+        public Block()
+        {
+            this.Arc = 0.0f;
+            this.Shield = 0;
+        }
+
+        public override void ReadInstance(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading Block Ability...");
+
+            this.Arc = reader.ReadSingle();
+            this.Shield = reader.ReadInt32();
+        }
+
+        public override void WriteInstance(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing Block Ability...");
+
+            writer.Write(this.Arc);
+            writer.Write(this.Shield);
+        }
+    }
+}

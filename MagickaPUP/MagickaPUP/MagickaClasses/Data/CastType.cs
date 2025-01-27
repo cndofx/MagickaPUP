fe00c4a785f55ba105a102b638be29ec338cac0a
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-27 19:05:22 +0100

    Created CastType enum

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Data/CastType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Data
+{
+    public enum CastType
+    {
+        None,
+        Blast = 0,
+        Force,
+        Area,
+        Self,
+        Weapon,
+        Magick
+    }
+}

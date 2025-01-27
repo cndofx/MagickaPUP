acc6641ba63c402a6d9914c7f1c0c0c4f5b8d0d1
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-27 19:12:09 +0100

    Implemented RemoveStatus Ability

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Abilities/Derived/RemoveStatus.cs
@@ -0,0 +1,25 @@
+﻿using MagickaPUP.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Abilities.Derived
+{
+    public class RemoveStatus : Ability
+    {
+        public RemoveStatus()
+        { }
+
+        public override void ReadInstance(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading RemoveStatus Ability...");
+        }
+
+        public override void WriteInstance(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing RemoveStatus Ability...");
+        }
+    }
+}

e8f369be687f1d54a98176cca9a28a6fa4be6255
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-28 17:44:26 +0100

    Implemented read and write operations for AuraBuff

--- MagickaPUP/MagickaPUP/MagickaClasses/Character/Aura/Derived/AuraBuff.cs
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Aura/Derived/AuraBuff.cs
@@ -4,23 +4,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MagickaPUP.MagickaClasses.Character.Buffs;
 
 namespace MagickaPUP.MagickaClasses.Character.Aura.Derived
 {
     // TODO : Implement
     public class AuraBuff : Aura
     {
-        public float Strength { get; set; }
+        public BuffStorage Buff { get; set; }
 
         public AuraBuff()
-        { }
+        {
+            this.Buff = new BuffStorage();
+        }
 
         public AuraBuff(MBinaryReader reader, DebugLogger logger = null)
-        { }
+        {
+            logger?.Log(1, "Reading AuraBuff...");
+
+            this.Buff = new BuffStorage(reader, logger);
+        }
 
         public override void Write(MBinaryWriter writer, DebugLogger logger = null)
         {
+            logger?.Log(1, "Writing AuraBuff...");
 
+            this.Buff.Write(writer, logger);
         }
     }
 }

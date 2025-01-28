0497b577edd98c518c47a0eba39418675314becb
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-28 03:53:06 +0100

    Implemented BuffResistance

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Buffs/Derived/BuffResistance.cs
@@ -0,0 +1,40 @@
+﻿using MagickaPUP.IO;
+using System;
+using System.Collections.Generic;
+using MagickaPUP.MagickaClasses.Data;
+
+namespace MagickaPUP.MagickaClasses.Character.Buffs.Derived
+{
+    public class BuffResistance : Buff
+    {
+        public Resistance Resistance { get; set; }
+
+        public BuffResistance()
+        {
+            this.Resistance = new Resistance();
+        }
+
+        public BuffResistance(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading BuffResistance...");
+
+            this.Resistance = new Resistance
+            {
+                elements = (Elements)reader.ReadInt32(),
+                modifier = reader.ReadSingle(),
+                multiplier = reader.ReadSingle(),
+                statusResistance = reader.ReadBoolean()
+            };
+        }
+
+        public override void Write(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing BuffResistance...");
+
+            writer.Write((int)this.Resistance.elements);
+            writer.Write((float)this.Resistance.modifier);
+            writer.Write((float)this.Resistance.multiplier);
+            writer.Write((bool)this.Resistance.statusResistance);
+        }
+    }
+}

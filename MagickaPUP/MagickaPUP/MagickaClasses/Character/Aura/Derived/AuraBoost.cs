abb1cbd1c133cf868f8cc46262f53b75ceb22370
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-28 17:42:15 +0100

    Implemented read and write operations for AuraBoost

--- MagickaPUP/MagickaPUP/MagickaClasses/Character/Aura/Derived/AuraBoost.cs
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Aura/Derived/AuraBoost.cs
@@ -10,15 +10,25 @@ namespace MagickaPUP.MagickaClasses.Character.Aura.Derived
     // TODO : Implement
     public class AuraBoost : Aura
     {
+        public float Magnitude { get; set; }
+
         public AuraBoost()
-        { }
+        {
+            this.Magnitude = 0.0f;
+        }
 
         public AuraBoost(MBinaryReader reader, DebugLogger logger = null)
-        { }
+        {
+            logger?.Log(1, "Reading AuraBoost...");
+
+            this.Magnitude = reader.ReadSingle();
+        }
 
         public override void Write(MBinaryWriter writer, DebugLogger logger = null)
         {
+            logger?.Log(1, "Writing AuraBoost...");
 
+            writer.Write(this.Magnitude);
         }
     }
 }

bdc7e6a6a539fcf4e3997fb24dc2310196f8c9e2
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-28 17:40:36 +0100

    Implemented read and write operations for AuraLove

--- MagickaPUP/MagickaPUP/MagickaClasses/Character/Aura/Derived/AuraLove.cs
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Aura/Derived/AuraLove.cs
@@ -10,15 +10,29 @@ namespace MagickaPUP.MagickaClasses.Character.Aura.Derived
     // TODO : Implement
     public class AuraLove : Aura
     {
+        public float Radius { get; set; }
+        public float Time { get; set; }
+
         public AuraLove()
-        { }
+        {
+            this.Radius = 1.0f;
+            this.Time = 1.0f;
+        }
 
         public AuraLove(MBinaryReader reader, DebugLogger logger = null)
-        { }
+        {
+            logger?.Log(1, "Reading AuraLove...");
+
+            this.Radius = reader.ReadSingle();
+            this.Time = reader.ReadSingle();
+        }
 
         public override void Write(MBinaryWriter writer, DebugLogger logger = null)
         {
+            logger?.Log(1, "Writing AuraLove...");
 
+            writer.Write(this.Radius);
+            writer.Write(this.Time);
         }
     }
 }

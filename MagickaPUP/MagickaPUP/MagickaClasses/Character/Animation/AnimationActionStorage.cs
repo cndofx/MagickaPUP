2f63ed6de5484263777c45176a3d55bfd70afe9e
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-28 23:18:18 +0100

    Created AnimationActionStorage class
    The old AnimationAction class turned out to be polymorphic as well, missed that originally while reverse engineering the class, so the easiest way to fix this problem is to move the generic logic into this storage class and then make AnimationAction into an abstract class.

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Animation/AnimationActionStorage.cs
@@ -0,0 +1,56 @@
+﻿using MagickaPUP.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Animation
+{
+    public class AnimationActionStorage
+    {
+        #region Variables
+
+        public string AnimationName { get; set; }
+        public float StartTime { get; set; }
+        public float EndTime { get; set; }
+
+        public AnimationAction AnimationAction { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public AnimationActionStorage()
+        { }
+
+        public AnimationActionStorage(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading AnimationActionStorage...");
+
+            this.AnimationName = reader.ReadString();
+            this.StartTime = reader.ReadSingle();
+            this.EndTime = reader.ReadSingle();
+
+            logger?.Log(2, $" - AnimationName : {this.AnimationName}");
+            logger?.Log(2, $" - StartTime     : {this.StartTime}");
+            logger?.Log(2, $" - EndTime       : {this.EndTime}");
+
+            // TODO : Implement AnimationAction reading
+            // this.AnimationAction
+        }
+
+        #endregion
+
+        #region PublicMethods
+
+        public void Write(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing AnimationActionStorage...");
+
+            throw new NotImplementedException("Write AnimationActionStorage is not implemented yet!");
+        }
+
+        #endregion
+    }
+}

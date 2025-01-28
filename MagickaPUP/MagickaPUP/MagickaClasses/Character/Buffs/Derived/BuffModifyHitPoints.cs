749c16a099beae5c7274cbeaaef531555d381576
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-28 03:52:27 +0100

    Implemented BuffModifyHitPoints

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Buffs/Derived/BuffModifyHitPoints.cs
@@ -0,0 +1,37 @@
+﻿using MagickaPUP.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Buffs.Derived
+{
+    public class BuffModifyHitPoints : Buff
+    {
+        public float HitPointsMultiplier { get; set; }
+        public float HitPointsModifier { get; set; }
+
+        public BuffModifyHitPoints()
+        {
+            this.HitPointsMultiplier = 1.0f;
+            this.HitPointsModifier = 0.0f;
+        }
+
+        public BuffModifyHitPoints(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading BuffModifyHitPoints...");
+
+            this.HitPointsMultiplier = reader.ReadSingle();
+            this.HitPointsModifier = reader.ReadSingle();
+        }
+
+        public override void Write(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing BuffModifyHitPoints...");
+
+            writer.Write(this.HitPointsMultiplier);
+            writer.Write(this.HitPointsModifier);
+        }
+    }
+}

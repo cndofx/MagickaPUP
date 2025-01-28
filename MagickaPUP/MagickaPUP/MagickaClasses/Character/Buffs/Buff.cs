6566ee81a000af68cbe81f0d6d86bc432ddb273a
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-28 03:51:55 +0100

    Created abstract Buff class

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Buffs/Buff.cs
@@ -0,0 +1,14 @@
+﻿using MagickaPUP.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Buffs
+{
+    public abstract class Buff
+    {
+        public abstract void Write(MBinaryWriter writer, DebugLogger logger = null);
+    }
+}

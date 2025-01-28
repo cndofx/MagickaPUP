2aadf4f2a56957d51b665b4db18d74e6e5520d43
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-28 07:19:41 +0100

    Finished implementing reading for AuraStorage class

--- MagickaPUP/MagickaPUP/MagickaClasses/Character/Aura/AuraStorage.cs
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Aura/AuraStorage.cs
@@ -4,9 +4,8 @@ using MagickaPUP.MagickaClasses.Data.Aura;
 using MagickaPUP.MagickaClasses.Generic;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MagickaPUP.Utility.Exceptions;
+using MagickaPUP.MagickaClasses.Character.Aura.Derived;
 
 namespace MagickaPUP.MagickaClasses.Character.Aura
 {
@@ -28,7 +27,43 @@ namespace MagickaPUP.MagickaClasses.Character.Aura
         { }
 
         public AuraStorage(MBinaryReader reader, DebugLogger logger = null)
-        { }
+        {
+            this.AuraTarget = (AuraTarget)reader.ReadByte();
+            this.AuraType = (AuraType)reader.ReadByte();
+            this.VisualCategory = (VisualCategory)reader.ReadByte();
+            this.Color = Vec3.Read(reader, logger);
+            this.Effect = reader.ReadString();
+            this.Time = reader.ReadSingle();
+            this.Radius = reader.ReadSingle();
+
+            // Read the comma separated target string and then split it and store it within our TargetTypes array.
+            this.TargetTypes = reader.ReadString().Split(new char[] { ',' });
+
+            this.TargetFaction = (Factions)reader.ReadInt32();
+
+            // Get the correct aura type
+            switch (this.AuraType)
+            {
+                case AuraType.Buff:
+                    this.Aura = new AuraBuff(reader, logger);
+                    break;
+                case AuraType.Deflect:
+                    this.Aura = new AuraDeflect(reader, logger);
+                    break;
+                case AuraType.Boost:
+                    this.Aura = new AuraBoost(reader, logger);
+                    break;
+                case AuraType.LifeSteal:
+                    this.Aura = new AuraLifeSteal(reader, logger);
+                    break;
+                case AuraType.Love:
+                    this.Aura = new AuraLove(reader, logger);
+                    break;
+                default:
+                    throw new MagickaLoadException("Unknown AuraType was read!");
+                    break; // lol
+            }
+        }
 
         public void Write(MBinaryWriter writer, DebugLogger logger = null)
         { }

3091e84f81c39fc7bed93db2826d39df1ceeac87
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-28 23:17:25 +0100

    Modified AnimationList class to no longer inherit from XnaObject

--- MagickaPUP/MagickaPUP/MagickaClasses/Character/Animation/AnimationList.cs
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Animation/AnimationList.cs
@@ -8,9 +8,9 @@ using System.Threading.Tasks;
 
 namespace MagickaPUP.MagickaClasses.Character.Animation
 {
-    // NOTE : This class does NOT exist within Magicka's code, this is an abstraction made to make things easier for me to handle and to make the serialized JSON look nicer.
+    // NOTE : This class does NOT exist within Magicka's code (same with AnimationActionStorage...), this is an abstraction made to make things easier for me to handle and to make the serialized JSON look nicer.
     // This may be a very shitty idea tho, but whatever, fuck it.
-    public class AnimationList : XnaObject
+    public class AnimationList
     {
         #region Constants
 
@@ -18,7 +18,7 @@ namespace MagickaPUP.MagickaClasses.Character.Animation
         // Within Magicka's code, this value is hard coded as 231. This used to be the case here as well, until I noticed that 231 is actually the
         // value of total animations that exist within the Animations enum. For correctness sake, this value is now modified to be taken
         // from the Animations enum instead, altough the result is exactly the same one as it was before...
-        private static readonly int TOTAL_ANIMATIONS = (int)Animations.totalanimations; /* 231 */
+        // private static readonly int TOTAL_ANIMATIONS = (int)Animations.totalanimations; /* 231 */
 
         #endregion
 
@@ -36,12 +36,7 @@ namespace MagickaPUP.MagickaClasses.Character.Animation
             this.animationClips = new AnimationActionClip[ 0 /* TOTAL_ANIMATIONS */];
         }
 
-        #endregion
-
-        #region PublicMethods
-
-        // TODO : Make sure that this shit is actually properly implemented and I didn't fuck up anywhere... bruh...
-        public override void ReadInstance(MBinaryReader reader, DebugLogger logger = null)
+        public AnimationList(MBinaryReader reader, DebugLogger logger = null)
         {
             logger?.Log(1, "Reading AnimationList...");
 
@@ -64,18 +59,15 @@ namespace MagickaPUP.MagickaClasses.Character.Animation
             this.animationClips = new AnimationActionClip[this.numAnimationClips];
             for (int i = 0; i < this.numAnimationClips; ++i)
             {
-                this.animationClips[i] = AnimationActionClip.Read(reader, logger);
+                this.animationClips[i] = new AnimationActionClip(reader, logger);
             }
         }
 
-        public static AnimationList Read(MBinaryReader reader, DebugLogger logger = null)
-        {
-            var ans = new AnimationList();
-            ans.ReadInstance(reader, logger);
-            return ans;
-        }
+        #endregion
 
-        public override void WriteInstance(MBinaryWriter writer, DebugLogger logger = null)
+        #region PublicMethods
+
+        public void Write(MBinaryWriter writer, DebugLogger logger = null)
         {
             logger?.Log(1, "Writing AnimationList...");
             throw new NotImplementedException("Write AnimationList is not implemented yet!");

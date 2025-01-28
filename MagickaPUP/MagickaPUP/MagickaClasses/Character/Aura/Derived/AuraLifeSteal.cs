399d0541e72629de264ec54bac6ffb7ad16331df
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-28 17:38:55 +0100

    Implemented read and write operations for AuraLifeSteal

--- MagickaPUP/MagickaPUP/MagickaClasses/Character/Aura/Derived/AuraLifeSteal.cs
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Aura/Derived/AuraLifeSteal.cs
@@ -10,15 +10,25 @@ namespace MagickaPUP.MagickaClasses.Character.Aura.Derived
     // TODO : Implement
     public class AuraLifeSteal : Aura
     {
+        public float Amount { get; set; }
+
         public AuraLifeSteal()
-        { }
+        {
+            this.Amount = 0.0f;
+        }
 
         public AuraLifeSteal(MBinaryReader reader, DebugLogger logger = null)
-        { }
+        {
+            logger?.Log(1, "Reading AuraLifeSteal...");
+
+            this.Amount = reader.ReadSingle();
+        }
 
         public override void Write(MBinaryWriter writer, DebugLogger logger = null)
         {
+            logger?.Log(1, "Writing AuraLifeSteal...");
 
+            writer.Write(this.Amount);
         }
     }
 }

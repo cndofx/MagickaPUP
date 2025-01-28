438e94cb5061d49cc1c0410a1224f9a3e33944a9
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-28 06:53:35 +0100

    Added AuraTarget and AuraType enums

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Data/Aura/AuraTarget.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Data.Aura
+{
+    // TODO : Figure out what this corresponds to. Is this the thing that is around you when you use wards? like, it seems like it affects entities around a given entity, so... could be it!
+    public enum AuraTarget : byte
+    {
+        Friendly,
+        FriendlyButSelf,
+        Enemy,
+        All,
+        AllButSelf,
+        Self,
+        Type,
+        TypeButSelf,
+        Faction,
+        FactionButSelf
+    }
+}
---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Data/Aura/AuraType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Data.Aura
+{
+    // TODO : Figure out what this does.
+    public enum AuraType : byte
+    {
+        Buff,
+        Deflect,
+        Boost,
+        LifeSteal,
+        Love
+    }
+}

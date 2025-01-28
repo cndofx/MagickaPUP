e2842a93233924f87edffdd19a444f8795c49fb7
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-28 07:01:11 +0100

    Added explanatory comment to AuraStorage

--- MagickaPUP/MagickaPUP/MagickaClasses/Character/Aura/AuraStorage.cs
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Aura/AuraStorage.cs
@@ -18,7 +18,7 @@ namespace MagickaPUP.MagickaClasses.Character.Aura
         public VisualCategory VisualCategory { get; set; }
         public Vec3 Color { get; set; }
         public string Effect { get; set; }
-        public float Time { get; set; }
+        public float Time { get; set; } // NOTE : The Execute() method within the AuraStorage class subtracts every frame delta time from its variable TTL, which is what this Time variable corresponds to. This means that this would most likely correspond to some kind of "Duration" variable. Maybe TTL means "Time to Last"? or whatever the fuck? lol...
         public float Radius { get; set; }
         public string[] TargetTypes { get; set; } // NOTE : This is a single string within XNB files. It contains a comma separated list of elements. For easier JSON editting, I'm splitting them into an array, but you MUST REMEMBER this detail when implementing both the read and write logic for this class!!! otherwise, everything goes to SHIT!
         public Factions TargetFaction { get; set; }

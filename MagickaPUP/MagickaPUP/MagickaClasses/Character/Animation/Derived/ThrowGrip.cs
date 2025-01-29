680ccaa90a94d77b80cb6cde4f606eea4e440c49
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-29 06:51:42 +0100

    Implemented ThrowGrip AnimationAction

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Animation/Derived/ThrowGrip.cs
@@ -0,0 +1,31 @@
+﻿using MagickaPUP.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Animation.Derived
+{
+    public class ThrowGrip : AnimationAction
+    {
+        // No fucking data to be seen here!
+
+        public ThrowGrip()
+        {
+            // Do nothing!
+        }
+
+        public ThrowGrip(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading ThrowGrip AnimationAction...");
+            // Nothing to see here, no data to be read, so do fuck all!
+        }
+
+        public override void Write(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing ThrowGrip AnimationAction...");
+            // Nothing to see here, no data to be written, so do fuck all!
+        }
+    }
+}

9a42809440e5f65cdecfbdab6553ad20a643c2b3
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-29 06:50:18 +0100

    Implemented Immortal AnimationAction

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Animation/Derived/Immortal.cs
@@ -0,0 +1,33 @@
+﻿using MagickaPUP.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Animation.Derived
+{
+    public class Immortal : AnimationAction
+    {
+        public bool Collide { get; set; }
+
+        public Immortal()
+        {
+            this.Collide = false;
+        }
+
+        public Immortal(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading Immortal AnimationAction...");
+
+            this.Collide = reader.ReadBoolean();
+        }
+
+        public override void Write(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing Immortal AnimationAction...");
+
+            writer.Write(this.Collide);
+        }
+    }
+}

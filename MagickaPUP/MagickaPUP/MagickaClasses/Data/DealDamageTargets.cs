8ec9eeebd7554761086a216b4439d0db41afe0ae
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-29 04:12:37 +0100

    Added DealDamageTargets enum

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Data/DealDamageTargets.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Data
+{
+    [Flags]
+    public enum DamageTargets : byte
+    {
+        None = 0,
+        Target = 0,
+        Friendly = 1,
+        Enemy = 2,
+        NonCharacters = 4,
+        All = 255
+    }
+}

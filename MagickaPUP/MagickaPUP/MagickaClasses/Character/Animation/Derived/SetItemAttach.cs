683117a0d3eff3d51aa0d7324f35ecb3ace72055
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-29 06:51:17 +0100

    Implemented SetItemAttach AnimationAction

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Animation/Derived/SetItemAttach.cs
@@ -0,0 +1,34 @@
+﻿using MagickaPUP.IO;
+using System;
+using System.Collections.Generic;
+
+namespace MagickaPUP.MagickaClasses.Character.Animation.Derived
+{
+    public class SetItemAttach : AnimationAction
+    {
+        public int Item { get; set; }
+        public string JointBone { get; set; }
+
+        public SetItemAttach()
+        {
+            this.Item = 0;
+            this.JointBone = string.Empty;
+        }
+
+        public SetItemAttach(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading SetItemAttach AnimationAction...");
+
+            this.Item = reader.ReadInt32();
+            this.JointBone = reader.ReadString();
+        }
+
+        public override void Write(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing SetItemAttach AnimationAction...");
+
+            writer.Write(this.Item);
+            writer.Write(this.JointBone);
+        }
+    }
+}

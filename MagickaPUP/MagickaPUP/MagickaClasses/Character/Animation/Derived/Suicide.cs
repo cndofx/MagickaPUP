69962bb5b6d570fdbe55f6346a616a84cd097b6a
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-29 06:51:36 +0100

    Implemented Suicide AnimationAction

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Animation/Derived/Suicide.cs
@@ -0,0 +1,33 @@
+﻿using MagickaPUP.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Animation.Derived
+{
+    public class Suicide : AnimationAction
+    {
+        public bool Overkill { get; set; }
+
+        public Suicide()
+        {
+            this.Overkill = false;
+        }
+
+        public Suicide(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading Suicide AnimationAction...");
+
+            this.Overkill = reader.ReadBoolean();
+        }
+
+        public override void Write(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing Suicide AnimationAction...");
+
+            writer.Write(this.Overkill);
+        }
+    }
+}

2231c4efb3bd4397de462775c9ce93fc90df740f
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-29 06:50:00 +0100

    Implemented Footstep AnimationAction

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Animation/Derived/Footstep.cs
@@ -0,0 +1,31 @@
+﻿using MagickaPUP.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Animation.Derived
+{
+    public class Footstep : AnimationAction
+    {
+        // Holds no data
+
+        public Footstep()
+        {
+            // Does nothing
+        }
+
+        public Footstep(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading Footstep AnimationAction...");
+            // Reads no further data
+        }
+
+        public override void Write(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing Footstep AnimationAction...");
+            // Writes no further data
+        }
+    }
+}

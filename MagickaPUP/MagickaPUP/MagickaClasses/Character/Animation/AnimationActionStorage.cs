92e1a1314b019db4bb291e14b0bbd462348abc79
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-29 15:24:56 +0100

    Implemented AnimationAction reading through AnimationActionType

--- MagickaPUP/MagickaPUP/MagickaClasses/Character/Animation/AnimationActionStorage.cs
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Animation/AnimationActionStorage.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using MagickaPUP.Utility.Exceptions;
 using MagickaPUP.MagickaClasses.Data;
+using MagickaPUP.MagickaClasses.Character.Animation.Derived;
 
 namespace MagickaPUP.MagickaClasses.Character.Animation
 {
@@ -10,7 +11,7 @@ namespace MagickaPUP.MagickaClasses.Character.Animation
     {
         #region Variables
 
-        public string AnimationActionType { get; set; }
+        public string ActionType { get; set; }
         public float StartTime { get; set; }
         public float EndTime { get; set; }
 
@@ -27,22 +28,106 @@ namespace MagickaPUP.MagickaClasses.Character.Animation
         {
             logger?.Log(1, "Reading AnimationActionStorage...");
 
-            this.AnimationActionType = reader.ReadString(); // TODO : Move this string out of this class or mark it as a non JSON serializable field, since it will be added again on the child AnimationAction's JSON object.
+            this.ActionType = reader.ReadString(); // TODO : Move this string out of this class or mark it as a non JSON serializable field, since it will be added again on the child AnimationAction's JSON object.
             this.StartTime = reader.ReadSingle();
             this.EndTime = reader.ReadSingle();
 
-            logger?.Log(2, $" - AnimationActionType : {this.AnimationActionType}");
+            logger?.Log(2, $" - AnimationActionType : {this.ActionType}");
             logger?.Log(2, $" - StartTime           : {this.StartTime}");
             logger?.Log(2, $" - EndTime             : {this.EndTime}");
 
             AnimationActionType type;
-            bool success = Enum.TryParse<AnimationActionType>(this.AnimationActionType, true, out type);
+            bool success = Enum.TryParse<AnimationActionType>(this.ActionType, true, out type);
             if (!success)
-                throw new MagickaLoadException($"Could not load the specified AnimationAction type. The type \"{this.AnimationActionType}\" is not a valid AnimationAction for Magicka.");
+                throw new MagickaLoadException($"Could not load the specified AnimationAction type. The type \"{this.ActionType}\" is not a valid AnimationAction for Magicka.");
 
+            // wtf... I hate these chains I swear... such a fucking pain in the ass to write in C#
             switch (type)
             {
-                // TODO : Implement...
+                case AnimationActionType.Block:
+                    this.AnimationAction = new Block(reader, logger);
+                    break;
+                case AnimationActionType.BreakFree:
+                    this.AnimationAction = new BreakFree(reader, logger);
+                    break;
+                case AnimationActionType.CameraShake:
+                    this.AnimationAction = new CameraShake(reader, logger);
+                    break;
+                case AnimationActionType.CastSpell:
+                    this.AnimationAction = new CastSpell(reader, logger);
+                    break;
+                case AnimationActionType.Crouch:
+                    this.AnimationAction = new Crouch(reader, logger);
+                    break;
+                case AnimationActionType.DamageGrip:
+                    this.AnimationAction = new DamageGrip(reader, logger);
+                    break;
+                case AnimationActionType.DealDamage:
+                    this.AnimationAction = new DealDamage(reader, logger);
+                    break;
+                case AnimationActionType.DetachItem:
+                    this.AnimationAction = new DetachItem(reader, logger);
+                    break;
+                case AnimationActionType.Ethereal:
+                    this.AnimationAction = new Ethereal(reader, logger);
+                    break;
+                case AnimationActionType.Footstep:
+                    this.AnimationAction = new Footstep(reader, logger);
+                    break;
+                case AnimationActionType.Grip:
+                    this.AnimationAction = new Grip(reader, logger);
+                    break;
+                case AnimationActionType.Gunfire:
+                    this.AnimationAction = new Gunfire(reader, logger);
+                    break;
+                case AnimationActionType.Immortal:
+                    this.AnimationAction = new Immortal(reader, logger);
+                    break;
+                case AnimationActionType.Invisible:
+                    this.AnimationAction = new Invisible(reader, logger);
+                    break;
+                case AnimationActionType.Jump:
+                    this.AnimationAction = new Jump(reader, logger);
+                    break;
+                case AnimationActionType.Move:
+                    this.AnimationAction = new Move(reader, logger);
+                    break;
+                case AnimationActionType.OverkillGrip:
+                    this.AnimationAction = new OverkillGrip(reader, logger);
+                    break;
+                case AnimationActionType.PlayEffect:
+                    this.AnimationAction = new PlayEffect(reader, logger);
+                    break;
+                case AnimationActionType.PlaySound:
+                    this.AnimationAction = new PlaySound(reader, logger);
+                    break;
+                case AnimationActionType.ReleaseGrip:
+                    this.AnimationAction = new ReleaseGrip(reader, logger);
+                    break;
+                case AnimationActionType.RemoveStatus:
+                    this.AnimationAction = new RemoveStatus(reader, logger);
+                    break;
+                case AnimationActionType.SetItemAttach:
+                    this.AnimationAction = new SetItemAttach(reader, logger);
+                    break;
+                case AnimationActionType.SpawnMissile:
+                    this.AnimationAction = new SpawnMissile(reader, logger);
+                    break;
+                case AnimationActionType.SpecialAbility:
+                    this.AnimationAction = new SpecialAbility(reader, logger);
+                    break;
+                case AnimationActionType.Suicide: // This is what I want to do right now :D
+                    this.AnimationAction = new Suicide(reader, logger);
+                    break;
+                case AnimationActionType.ThrowGrip:
+                    this.AnimationAction = new ThrowGrip(reader, logger);
+                    break;
+                case AnimationActionType.Tongue:
+                    this.AnimationAction = new Tongue(reader, logger);
+                    break;
+                case AnimationActionType.WeaponVisibility:
+                    this.AnimationAction = new WeaponVisibility(reader, logger);
+                    break;
             }
         }
 

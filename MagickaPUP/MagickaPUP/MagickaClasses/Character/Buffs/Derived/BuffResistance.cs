78ce45e190e726ac1366bc8ea1e1098942bfc8b4
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-29 20:21:23 +0100

    Updated BuffResistance to use Resistance's reader and writer methods
    Rather than reading and writing the resistance's parameters inline, we now use the resistance's own built in read and write methods.

--- MagickaPUP/MagickaPUP/MagickaClasses/Character/Buffs/Derived/BuffResistance.cs
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Buffs/Derived/BuffResistance.cs
@@ -18,23 +18,14 @@ namespace MagickaPUP.MagickaClasses.Character.Buffs.Derived
         {
             logger?.Log(1, "Reading BuffResistance...");
 
-            this.Resistance = new Resistance
-            {
-                elements = (Elements)reader.ReadInt32(),
-                modifier = reader.ReadSingle(),
-                multiplier = reader.ReadSingle(),
-                statusResistance = reader.ReadBoolean()
-            };
+            this.Resistance = new Resistance(reader, logger);
         }
 
         public override void Write(MBinaryWriter writer, DebugLogger logger = null)
         {
             logger?.Log(1, "Writing BuffResistance...");
 
-            writer.Write((int)this.Resistance.elements);
-            writer.Write((float)this.Resistance.modifier);
-            writer.Write((float)this.Resistance.multiplier);
-            writer.Write((bool)this.Resistance.statusResistance);
+            this.Resistance.Write(writer, logger);
         }
     }
 }

03a3830b3aef494e0b4ed486155f809179431f49
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-29 01:06:06 +0100

    Implemented CameraShake AnimationAction

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Animation/Derived/CameraShake.cs
@@ -0,0 +1,41 @@
+﻿using MagickaPUP.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Animation.Derived
+{
+    public class CameraShake : AnimationAction
+    {
+        public string Text { get; set; }
+        public float Duration { get; set; }
+        public float Magnitude { get; set; }
+
+        public CameraShake()
+        {
+            this.Text = string.Empty;
+            this.Duration = 0.0f;
+            this.Magnitude = 0.0f;
+        }
+
+        public CameraShake(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading CameraShake AnimationAction...");
+
+            this.Text = reader.ReadString();
+            this.Duration = reader.ReadSingle();
+            this.Magnitude = reader.ReadSingle();
+        }
+
+        public override void Write(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing CameraShake AnimationAction...");
+
+            writer.Write(this.Text);
+            writer.Write(this.Duration);
+            writer.Write(this.Magnitude);
+        }
+    }
+}

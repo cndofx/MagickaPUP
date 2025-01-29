04621416b050ef68b84b5b41fd2ed4637ca5751f
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-29 06:50:35 +0100

    Implemented Move AnimationAction

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Animation/Derived/Move.cs
@@ -0,0 +1,34 @@
+﻿using MagickaPUP.IO;
+using MagickaPUP.MagickaClasses.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Animation.Derived
+{
+    public class Move : AnimationAction
+    {
+        public Vec3 Velocity { get; set; }
+
+        public Move()
+        {
+            this.Velocity = new Vec3();
+        }
+
+        public Move(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading Move AnimationAction...");
+
+            this.Velocity = Vec3.Read(reader, logger);
+        }
+
+        public override void Write(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing Move AnimationAction...");
+
+            this.Velocity.WriteInstance(writer, logger);
+        }
+    }
+}

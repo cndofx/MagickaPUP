0f0a42e12aeb92db1681dd2fcfe2fc68a6b39767
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-29 01:05:47 +0100

    Implemented Block AnimationAction

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Animation/Derived/Block.cs
@@ -0,0 +1,33 @@
+﻿using MagickaPUP.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Animation.Derived
+{
+    public class Block : AnimationAction
+    {
+        public int Weapon { get; set; }
+
+        public Block()
+        {
+            this.Weapon = 0;
+        }
+
+        public Block(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading Block AnimationAction...");
+
+            this.Weapon = reader.ReadInt32();
+        }
+
+        public override void Write(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing BlockAnimationAction...");
+
+            writer.Write(this.Weapon);
+        }
+    }
+}

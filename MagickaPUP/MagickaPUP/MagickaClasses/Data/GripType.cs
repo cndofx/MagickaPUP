0d6c3683f7bec6955f7134e1f1f3bc71094a8870
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-29 04:12:25 +0100

    Added GripType enum

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Data/GripType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Data
+{
+    public enum GripType
+    {
+        Pickup,
+        Ride,
+        Hold
+    }
+}

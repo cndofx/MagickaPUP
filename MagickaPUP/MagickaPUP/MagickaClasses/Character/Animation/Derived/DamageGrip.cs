597ca5e5b6474b2ba854cd7f6e1aaa2753d8e6c6
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-29 01:53:17 +0100

    Created DamageGrip AnimationAction and implemented reading
    TODO : Implement writing

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Animation/Derived/DamageGrip.cs
@@ -0,0 +1,49 @@
+﻿using MagickaPUP.IO;
+using System;
+using System.Collections.Generic;
+using MagickaPUP.Utility.Exceptions;
+using MagickaPUP.MagickaClasses.Data;
+
+namespace MagickaPUP.MagickaClasses.Character.Animation.Derived
+{
+    public class DamageGrip : AnimationAction
+    {
+        public bool DamageOwner { get; set; } // Maybe rename to "DamageAffectsOwner"
+        public int NumDamages { get; set; } // Maybe get rid of this field? we don't really need it... I'm being pretty inconsistent regarding when I keep it and when I discard it tbh lol...
+        public Damage[] Damages { get; set; } 
+
+        public DamageGrip()
+        {
+            this.DamageOwner = false;
+            this.NumDamages = 0;
+            this.Damages = Array.Empty<Damage>();
+        }
+
+        public DamageGrip(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading DamageGrip AnimationAction...");
+
+            this.DamageOwner = reader.ReadBoolean();
+            this.NumDamages = reader.ReadInt32();
+            if (this.NumDamages > 5)
+                throw new MagickaLoadException($"Magicka does not support more than 5 damage entries for a DamageGrip! ({this.NumDamages} were found)");
+            this.Damages = new Damage[this.NumDamages];
+            for (int i = 0; i < this.NumDamages; ++i)
+            {
+                this.Damages[i] = new Damage
+                {
+                    AttackProperty = (AttackProperties)reader.ReadInt32(),
+                    Element = (Elements)reader.ReadInt32(),
+                    Amount = reader.ReadSingle(),
+                    Magnitude = reader.ReadSingle()
+                };
+            }
+        }
+
+        public override void Write(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing DamageGrip AnimationAction...");
+            throw new NotImplementedException("Write DamageGrip AnimationAction is not implemented yet!");
+        }
+    }
+}

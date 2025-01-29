56b4f2d1bade66d048cb330cb873007338834e5d
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-29 06:50:25 +0100

    Implemented Invisible AnimationAction

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Animation/Derived/Invisible.cs
@@ -0,0 +1,33 @@
+﻿using MagickaPUP.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Animation.Derived
+{
+    public class Invisible : AnimationAction
+    {
+        public bool NoEffect { get; set; }
+
+        public Invisible()
+        {
+            this.NoEffect = false;
+        }
+
+        public Invisible(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading Invisible AnimationAction...");
+
+            this.NoEffect = reader.ReadBoolean();
+        }
+
+        public override void Write(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing Invisible AnimationAction...");
+
+            writer.Write(this.NoEffect);
+        }
+    }
+}

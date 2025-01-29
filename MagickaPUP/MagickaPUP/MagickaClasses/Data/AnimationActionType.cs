ac3a5381fa3756ccc515fbf577a108f8eef57b35
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-29 15:13:54 +0100

    Created AnimationActionType enum
    This enum will be used to make parsing the animation action type strings easier when reading XNB files.
    
    Note that JSON parsing with C#'s .NET JsonSerializer should use a separate tag for polymorphic types, but the string should be the same in a case insesitive manner (since we're going to parse the enum with case insensitive settings, since Magicka's internal functionality for loading AnimationActions does so with InvariantCultureIgnoreCase)

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Data/AnimationActionType.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Data
+{
+    // NOTE : This does not exist in the base game, this is just an enum that I use to make parsing AnimationAction strings easier for myself.
+    public enum AnimationActionType
+    {
+        Block,
+        BreakFree,
+        CameraShake,
+        CastSpell,
+        Crouch,
+        DamageGrip,
+        DealDamage,
+        DetachItem,
+        Ethereal,
+        Footstep,
+        Grip,
+        Gunfire,
+        Immortal,
+        Invisible,
+        Jump,
+        Move,
+        OverkillGrip,
+        PlayEffect,
+        PlaySound,
+        ReleaseGrip,
+        RemoveStatus,
+        SetItemAttach,
+        SpawnMissile,
+        SpecialAbility,
+        Suicide,
+        ThrowGrip,
+        Tongue,
+        WeaponVisibility
+    }
+}

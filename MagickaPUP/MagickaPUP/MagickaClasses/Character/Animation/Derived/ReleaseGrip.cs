d655cebda4753a46c594f1a3fc6af10e8b46c326
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-29 06:51:03 +0100

    Implemented ReleaseGrip AnimationAction

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Character/Animation/Derived/ReleaseGrip.cs
@@ -0,0 +1,31 @@
+﻿using MagickaPUP.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Character.Animation.Derived
+{
+    public class ReleaseGrip : AnimationAction
+    {
+        // Holds no data
+
+        public ReleaseGrip()
+        {
+            // Does nothing
+        }
+
+        public ReleaseGrip(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading ReleaseGrip AnimationAction...");
+            // Reads no further data
+        }
+
+        public override void Write(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing ReleaseGrip AnimationAction...");
+            // Writes no further data
+        }
+    }
+}

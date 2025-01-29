9f5396ddeeb3801a3de3ddbe4d44c7da73ed87a0
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-29 06:52:07 +0100

    Added StatusEffect enum

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Data/StatusEffect.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Data
+{
+    [Flags]
+    public enum StatusEffects : short
+    {
+        None = 0,
+        Burning = 1,
+        Wet = 2,
+        Frozen = 4,
+        Cold = 8,
+        Poisoned = 16,
+        Healing = 32,
+        Life = 32,
+        Greased = 64,
+        Steamed = 128,
+        Bleeding = 256,
+        NumberOfTypes = 512
+    }
+}

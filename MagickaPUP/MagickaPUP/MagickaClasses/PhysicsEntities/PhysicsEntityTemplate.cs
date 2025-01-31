9a013e53302e2344ee020c81135bc8b64dc7bdf3
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-31 21:08:02 +0100

    Created PhysicsEntityTemplate class

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/PhysicsEntities/PhysicsEntityTemplate.cs
@@ -0,0 +1,22 @@
+﻿using MagickaPUP.XnaClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.PhysicsEntities
+{
+    public class PhysicsEntityTemplate : XnaObject
+    {
+        #region Variables
+
+        #endregion
+
+        #region PublicMethods
+        #endregion
+
+        #region PrivateMethods
+        #endregion
+    }
+}

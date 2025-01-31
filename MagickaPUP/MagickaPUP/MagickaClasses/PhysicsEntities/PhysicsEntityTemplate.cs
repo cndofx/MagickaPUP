cdef75789c6d0b2fd3415583adf9e6f8d4011e53
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-01-31 21:09:50 +0100

    Added WriteInstance and ReadInstance methods to PhysicsEntityTemplate class

--- MagickaPUP/MagickaPUP/MagickaClasses/PhysicsEntities/PhysicsEntityTemplate.cs
+++ MagickaPUP/MagickaPUP/MagickaClasses/PhysicsEntities/PhysicsEntityTemplate.cs
@@ -1,4 +1,5 @@
-﻿using MagickaPUP.XnaClasses;
+﻿using MagickaPUP.Utility.IO;
+using MagickaPUP.XnaClasses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,19 @@ namespace MagickaPUP.MagickaClasses.PhysicsEntities
         #endregion
 
         #region PublicMethods
+
+        public override void ReadInstance(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading PhysicsEntityTemplate...");
+            throw new NotImplementedException("Read PhysicsEntityTemplate is not implemented yet!");
+        }
+
+        public override void WriteInstance(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing PhysicsEntityTemplate...");
+            throw new NotImplementedException("Write PhysicsEntityTemplate is not implemented yet!");
+        }
+
         #endregion
 
         #region PrivateMethods

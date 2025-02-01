60a46c1c4b8fc982d5d55df052616c2a4953b021
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-02-01 02:51:47 +0100

    Created Advanced Settings class for Physics Entities

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/PhysicsEntities/AdvancedSettings.cs
@@ -0,0 +1,53 @@
+﻿using MagickaPUP.MagickaClasses.Character;
+using MagickaPUP.Utility.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.PhysicsEntities
+{
+    public class AdvancedSettings
+    {
+        // Advanced Config
+        public float Radius { get; set; }
+
+        // Model Properties
+        public ModelProperties[] ModelProperties { get; set; }
+
+        // Skinned Mesh
+        public bool HasSkinnedMesh { get; set; }
+        public string SkinnedMesh { get; set; } // External Reference. Contains the skeleton / animations etc...
+
+        public AdvancedSettings()
+        { }
+
+        public AdvancedSettings(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading Advanced Physics Entity Settings...");
+
+            // Advanced Config
+            this.Radius = reader.ReadSingle();
+
+            // Model Properties
+            int numModelProperties = reader.ReadInt32();
+            this.ModelProperties = new ModelProperties[numModelProperties];
+            for (int i = 0; i < numModelProperties; ++i)
+                this.ModelProperties[i] = new ModelProperties(reader, logger);
+
+            // Skinned Mesh
+            this.HasSkinnedMesh = reader.ReadBoolean();
+            this.SkinnedMesh = reader.ReadString(); // ER
+
+            throw new NotImplementedException("Read Advanced Physics Entity Settings is not implemented yet!");
+        }
+
+        public void Write(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing Advanced Physics Entity Settings...");
+
+            throw new NotImplementedException("Write Advanced Physics Entity Settings is not implemented yet!");
+        }
+    }
+}

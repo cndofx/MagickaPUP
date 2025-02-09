cf16a69cf4e6d27d8a113283a123e160692d56a1
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-02-09 16:03:48 +0100

    Created IPup interface

---
+++ MagickaPUP/MagickaPUP/Core/IPup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.Core
+{
+    // This interface's purpose is to standardise the functionality for the packer and unpacker classes, since they both take the same parameters and are operations of the same kind, where a file of a given input type is converted into a file of an output type.
+    // eg: JSON <-> XNB
+    // NOTE : Maybe rename to ICompiler since Pup is kind of a weird name...
+    public interface IPup
+    {
+        public int Compile(string inputFile, string outputFile); // NOTE : The int return type is kind of "obsolete" but we still preserve it for now.
+    }
+}

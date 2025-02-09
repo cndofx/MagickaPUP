4045ae0da390f62f9fad1228d8f36da5b8195ce5
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-02-09 13:17:09 +0100

    Moved CmdEntry struct to its own individual file

--- MagickaPUP/MagickaPUP/Core/PupProgram.cs
+++ MagickaPUP/MagickaPUP/Core/PupProgram.cs
@@ -18,30 +18,6 @@ namespace MagickaPUP.Core
 
         #endregion
 
-        #region Structs and Classes
-
-        public struct CmdEntry
-        {
-            public string cmd1; // short command name
-            public string cmd2; // long command name
-            public string desc1; // arguments
-            public string desc2; // description
-            public int args;
-            public Action<string[], int> fn;
-
-            public CmdEntry(string cmd1, string cmd2, string desc1, string desc2, int args, Action<string[], int> fn)
-            {
-                this.cmd1 = cmd1;
-                this.cmd2 = cmd2;
-                this.desc1 = desc1;
-                this.desc2 = desc2;
-                this.args = args;
-                this.fn = fn;
-            }
-        }
-
-        #endregion
-
         #region Variables
 
         private List<Packer> packers;

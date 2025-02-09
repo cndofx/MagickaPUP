33677709aff153fbfd28acfd367376b00b98b9f8
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-02-09 19:22:30 +0100

    Modified CmdEntry struct to work like the implementation we had this fucking morning
    The new thing I tried just turned out to be overengineered trash, plain and simple...

--- MagickaPUP/MagickaPUP/Core/Args/CmdEntry.cs
+++ MagickaPUP/MagickaPUP/Core/Args/CmdEntry.cs
@@ -2,8 +2,7 @@
 
 namespace MagickaPUP.Core.Args
 {
-    public delegate void CmdRegisterFunction(string[] args, int index);
-    public delegate void CmdExecuteFunction();
+    public delegate void CmdFunction(string[] args, int index);
 
     public struct CmdEntry
     {
@@ -12,30 +11,16 @@ namespace MagickaPUP.Core.Args
         public string desc1; // arguments
         public string desc2; // description
         public int args; // num args
-        public CmdRegisterFunction register; // Register Function (adds the command to the execution stack)
-        // public CmdExecuteFunction execute; // Execute Function (executes the function)
+        public CmdFunction fn; // Function that is executed when running the command
 
-        public CmdEntry(string cmd1, string cmd2, string desc1, string desc2, int args, CmdRegisterFunction register/*, CmdExecuteFunction execute*/)
+        public CmdEntry(string cmd1, string cmd2, string desc1, string desc2, int args, CmdFunction fn)
         {
             this.cmd1 = cmd1;
             this.cmd2 = cmd2;
             this.desc1 = desc1;
             this.desc2 = desc2;
             this.args = args;
-            this.register = register;
-            // this.execute = execute;
-        }
-    }
-
-    public struct CmdEntryExec
-    {
-        public int priority;
-        public CmdExecuteFunction execute;
-
-        public CmdEntryExec(int priority, CmdExecuteFunction fn)
-        {
-            this.priority = priority;
-            this.execute = fn;
+            this.fn = fn;
         }
     }
 }

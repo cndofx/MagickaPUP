08ac223e963e5a9078d9f921455f434ffee8f325
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-02-09 12:50:59 +0100

    Changed Packer namespace to MagickaPUP.Core

--- MagickaPUP/MagickaPUP/Core/Packer.cs
+++ MagickaPUP/MagickaPUP/Core/Packer.cs
@@ -1,15 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Text.Json;
 using MagickaPUP.XnaClasses;
-using MagickaPUP.MagickaClasses.Map;
 using MagickaPUP.Utility.IO;
 
-namespace MagickaPUP
+namespace MagickaPUP.Core
 {
     class Packer
     {
--- MagickaPUP/MagickaPUP/Program.cs
+++ MagickaPUP/MagickaPUP/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MagickaPUP.Core;
+using System;
 using System.Collections.Generic;
 using System.IO;
 

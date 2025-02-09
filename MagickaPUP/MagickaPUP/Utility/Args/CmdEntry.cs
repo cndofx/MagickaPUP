958dc805ad992a25fccb16f275a074f5a247c9fd
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-02-09 15:15:43 +0100

    Modified CmdEntry struct to use a custom delegate CmdFunction

--- MagickaPUP/MagickaPUP/Utility/Args/CmdEntry.cs
+++ MagickaPUP/MagickaPUP/Utility/Args/CmdEntry.cs
@@ -2,23 +2,25 @@
 
 namespace MagickaPUP.Utility.Args
 {
+    public delegate void CmdFunction(string[] args, int index);
+
     public struct CmdEntry
     {
         public string cmd1; // short command name
         public string cmd2; // long command name
         public string desc1; // arguments
         public string desc2; // description
-        public int args;
-        public Action<string[], int> fn;
+        public int args; // num args
+        public CmdFunction register; // Register Function (adds the command to the execution stack)
 
-        public CmdEntry(string cmd1, string cmd2, string desc1, string desc2, int args, Action<string[], int> fn)
+        public CmdEntry(string cmd1, string cmd2, string desc1, string desc2, int args, CmdFunction register)
         {
             this.cmd1 = cmd1;
             this.cmd2 = cmd2;
             this.desc1 = desc1;
             this.desc2 = desc2;
             this.args = args;
-            this.fn = fn;
+            this.register = register;
         }
     }
 }

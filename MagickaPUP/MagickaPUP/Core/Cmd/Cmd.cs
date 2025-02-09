e525a8ff177c6a78074793247aaeec28e70153ee
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-02-09 17:49:15 +0100

    Created abstract Cmd classes

---
+++ MagickaPUP/MagickaPUP/Core/Cmd/Cmd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.Core.Cmd
+{
+    // This class describes the base class to register a command for execution
+    public abstract class CmdRegisterBase
+    {
+
+    }
+
+    // This class describes the base class to execute a command that was registered
+    public abstract class CmdExecuteBase
+    {
+
+    }
+}

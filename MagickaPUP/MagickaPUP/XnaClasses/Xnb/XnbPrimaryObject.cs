1738299a15a998e7a2986e6d8373e2e4da3dc376
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-02-13 17:46:01 +0100

    Added explanatory comments to XnbPrimaryObject

--- MagickaPUP/MagickaPUP/XnaClasses/Xnb/XnbPrimaryObject.cs
+++ MagickaPUP/MagickaPUP/XnaClasses/Xnb/XnbPrimaryObject.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace MagickaPUP.XnaClasses.Xnb
 {
+    // NOTE : This class is pretty much a dummy for XnaObject, like wtf, as of now it's pretty useless...
+    // TODO : Maybe get rid of it?
     public class XnbPrimaryObject : XnaObject
     {
     }

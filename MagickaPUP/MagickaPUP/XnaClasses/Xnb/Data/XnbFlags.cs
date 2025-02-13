e76f9794b2d269272549b5b9c4a08620b341627b
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-02-13 21:44:36 +0100

    Created XNB Flags file

---
+++ MagickaPUP/MagickaPUP/XnaClasses/Xnb/Data/XnbFlags.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.XnaClasses.Xnb.Data
+{
+    [Flags]
+    public enum XnbFlags : byte
+    {
+        None = 0,
+        HiDefProfile = 0x01,
+        Lz4Compressed = 0x40, // 64
+        LzxCompressed = 0x80, // 128
+        Compressed = LzxCompressed | Lz4Compressed
+    }
+}

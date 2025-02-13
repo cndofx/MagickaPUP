970ab7180ad9a83930f065606f3b1ae7f19af87d
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-02-13 17:44:16 +0100

    Created basic logic for the XnbHeader class

--- MagickaPUP/MagickaPUP/XnaClasses/Xnb/XnbHeader.cs
+++ MagickaPUP/MagickaPUP/XnaClasses/Xnb/XnbHeader.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MagickaPUP.Utility.IO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,22 @@ using System.Threading.Tasks;
 
 namespace MagickaPUP.XnaClasses.Xnb
 {
-    public class XnbHeader
+    // TODO : Finish implementing (move the logic from the reader and writer classes into this one here!)
+    public class XnbHeader : XnaObject
     {
+        public XnbHeader()
+        { }
+
+        public override void ReadInstance(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading XnbHeader...");
+            throw new NotImplementedException("Read XnbHeader is not implemented yet!");
+        }
+
+        public override void WriteInstance(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing XnbHeader...");
+            throw new NotImplementedException("Write XnbHeader is not implemented yet!");
+        }
     }
 }

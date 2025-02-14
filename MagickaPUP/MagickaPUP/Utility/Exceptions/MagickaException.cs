9f4a8f65ca4d1249c06a294c3d62288009e613fa
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-02-14 02:34:01 +0100

    Created base exception class for Magicka related exceptions

---
+++ MagickaPUP/MagickaPUP/Utility/Exceptions/MagickaException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.Utility.Exceptions
+{
+    public class MagickaException : Exception
+    {
+        protected static readonly string EXCEPTION_MSG_BASE = "Magicka Exception";
+        protected static readonly string EXCEPTION_MSG_BODY = "An error occurred while performing an operation";
+
+        public MagickaException()
+        : base($"{EXCEPTION_MSG_BASE} : {EXCEPTION_MSG_BODY}")
+        { }
+
+        public MagickaException(string message)
+        : base($"{EXCEPTION_MSG_BASE} : {message}")
+        { }
+
+        public MagickaException(string message, Exception inner)
+        : base($"{EXCEPTION_MSG_BASE} : {message}", inner)
+        { }
+    }
+}

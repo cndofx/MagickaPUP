d16ec208149685c1d47798af977a5190d196db4e
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-02-14 16:05:10 +0100

    Removed old unused code from MBinaryWriter

--- MagickaPUP/MagickaPUP/Utility/IO/MWriter.cs
+++ MagickaPUP/MagickaPUP/Utility/IO/MWriter.cs
@@ -9,50 +9,16 @@ namespace MagickaPUP.Utility.IO
 {
     public class MBinaryWriter : BinaryWriter/*, IMWriter*/
     {
-        private int scopeDepth;
-        private string newlineString;
-        private string indentString;
+
 
         public MBinaryWriter(Stream stream) : base(stream)
         {
-            this.scopeDepth = 0;
-            this.newlineString = "\r\n";
-            this.indentString = "\t";
+
         }
 
         public new void Write7BitEncodedInt(int x)
         {
             base.Write7BitEncodedInt(x);
-        }
-
-        public void WriteRaw(string s)
-        {
-            byte[] bytes = Encoding.UTF8.GetBytes(s);
-            Write(bytes);
-        }
-
-        public void WriteText(string s)
-        {
-            WriteIndent();
-            WriteRaw($"{s}{this.newlineString}");
-        }
-
-        public void WriteScopeStart()
-        {
-            WriteText("{");
-            ++scopeDepth;
-        }
-
-        public void WriteScopeEnd(string s = "")
-        {
-            --scopeDepth;
-            WriteText($"}}{s}");
-        }
-
-        private void WriteIndent()
-        {
-            for (int i = 0; i < this.scopeDepth; ++i)
-                WriteRaw(indentString);
         }
     }
 

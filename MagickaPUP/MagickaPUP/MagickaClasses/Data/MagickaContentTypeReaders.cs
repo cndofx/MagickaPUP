fd82f1dfa10643705d754106ffb095afaef479ac
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-02-14 17:32:01 +0100

    Modified data layout in MagickaContentTypeReaders static class

--- MagickaPUP/MagickaPUP/MagickaClasses/Data/MagickaContentTypeReaders.cs
+++ MagickaPUP/MagickaPUP/MagickaClasses/Data/MagickaContentTypeReaders.cs
@@ -9,6 +9,32 @@ namespace MagickaPUP.MagickaClasses.Data
 {
     public static class MagickaContentTypeReaders
     {
+        // PolygonHead Readers
+        public static readonly ContentTypeReader BiTreeModelReader = new ContentTypeReader("PolygonHead.Pipeline.BiTreeModelReader, PolygonHead", 0);
+        public static readonly ContentTypeReader EffectDeferredReader = new ContentTypeReader("PolygonHead.Pipeline.RenderDeferredEffectReader, PolygonHead, Version=1.0.0.0, Culture=neutral", 0);
+        public static readonly ContentTypeReader EffectAdditiveReader = new ContentTypeReader("PolygonHead.Pipeline.AdditiveEffectReader, PolygonHead, Version=1.0.0.0, Culture=neutral", 0);
+        public static readonly ContentTypeReader EffectLiquidReader = new ContentTypeReader("PolygonHead.Pipeline.RenderDeferredLiquidEffectReader, PolygonHead", 0);
+        public static readonly ContentTypeReader EffectLavaReader = new ContentTypeReader("PolygonHead.Pipeline.LavaEffectReader, PolygonHead, Version=1.0.0.0, Culture=neutral", 0);
+
+
+        // Magicka Readers
+        public static readonly ContentTypeReader LevelModelReader = new ContentTypeReader("Magicka.ContentReaders.LevelModelReader, Magicka", 0);
+        public static readonly ContentTypeReader CharacterTemplateReader = new ContentTypeReader("Magicka.ContentReaders.CharacterTemplateReader, Magicka, Version=1.0.0.0, Culture=neutral", 0);
+        public static readonly ContentTypeReader PhysicsEntityTemplateReader = new ContentTypeReader("Magicka.ContentReaders.PhysicsEntityTemplateReader, Magicka", 0);
+
+        /*
+        public enum ContentTypeReaderIndex
+        {
+            BiTreeModelReader = 0,
+            EffectDeferredReader,
+            EffectAdditiveReader,
+            EffectLiquidReader,
+            EffectLavaReader,
+            LevelModelReader,
+            CharacterTemplateReader,
+            PhysicsEntityTemplateReader,
+        }
+
         public static readonly ContentTypeReader[] ContentTypeReaders = {
             // PolygonHead Readers
             new ContentTypeReader("PolygonHead.Pipeline.BiTreeModelReader, PolygonHead", 0),
@@ -22,5 +48,6 @@ namespace MagickaPUP.MagickaClasses.Data
             new ContentTypeReader("Magicka.ContentReaders.CharacterTemplateReader, Magicka, Version=1.0.0.0, Culture=neutral", 0),
             new ContentTypeReader("Magicka.ContentReaders.PhysicsEntityTemplateReader, Magicka", 0),
         };
+        */
     }
 }

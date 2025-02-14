5e579798f9a3fbadc24618c5822c4727852601df
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-02-14 16:07:54 +0100

    Added content type readers handling to MBinaryWriter

--- MagickaPUP/MagickaPUP/Utility/IO/MWriter.cs
+++ MagickaPUP/MagickaPUP/Utility/IO/MWriter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MagickaPUP.XnaClasses;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,17 +10,29 @@ namespace MagickaPUP.Utility.IO
 {
     public class MBinaryWriter : BinaryWriter/*, IMWriter*/
     {
+        #region Variables
 
+        public List<ContentTypeReader> ContentTypeReaders { get; private set; }
+
+        #endregion
+
+        #region Constructor
 
         public MBinaryWriter(Stream stream) : base(stream)
         {
-
+            this.ContentTypeReaders = new List<ContentTypeReader>();
         }
+
+        #endregion
+
+        #region PublicMethods
 
         public new void Write7BitEncodedInt(int x)
         {
             base.Write7BitEncodedInt(x);
         }
+
+        #endregion
     }
 
     public class MTextWriter : StreamWriter

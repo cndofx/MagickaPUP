bf7c014d779c64fa9f4eb81be0413ea905c34a4c
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-02-14 01:30:01 +0100

    Removed TODO comment and unused old methods from XnbFile class

--- MagickaPUP/MagickaPUP/XnaClasses/Xnb/XnbFile.cs
+++ MagickaPUP/MagickaPUP/XnaClasses/Xnb/XnbFile.cs
@@ -139,19 +139,6 @@ namespace MagickaPUP.XnaClasses.Xnb
             logger?.Log(1, "Finished writing XNB file!");
         }
 
-        public void SetPrimaryObject(XnaObject obj)
-        {
-            this.PrimaryObject = obj;
-        }
-
-        public void AddSharedResource(XnaObject obj)
-        {
-            this.SharedResources.Add(obj);
-        }
-
-        // MAYBE TODO : Maybe move all of the Packer and Unpacker logic to the Read and Write functions of this object? and we could also add other data such as the xnb header stuff as private or non json serializable variables and whatnot...
-        // INDEED, It would make quite a lot of sense to move the read and write logic to this object rigth here to make things easier, including the header and stuff like that...
-
         #endregion
 
         #region PrivateMethods - Write

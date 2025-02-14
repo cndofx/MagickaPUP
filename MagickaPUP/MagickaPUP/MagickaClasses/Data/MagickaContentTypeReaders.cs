34b0773faf9b44f847009e814719bc959a450a62
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-02-14 16:02:20 +0100

    Added classes with static lists of content type readers for Xna content and Magicka content
    TODO : Clean this up later on

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Data/MagickaContentTypeReaders.cs
@@ -0,0 +1,26 @@
+﻿using MagickaPUP.XnaClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Data
+{
+    public static class MagickaContentTypeReaders
+    {
+        public static readonly ContentTypeReader[] ContentTypeReaders = {
+            // PolygonHead Readers
+            new ContentTypeReader("PolygonHead.Pipeline.BiTreeModelReader, PolygonHead", 0),
+            new ContentTypeReader("PolygonHead.Pipeline.RenderDeferredEffectReader, PolygonHead, Version=1.0.0.0, Culture=neutral", 0),
+            new ContentTypeReader("PolygonHead.Pipeline.AdditiveEffectReader, PolygonHead, Version=1.0.0.0, Culture=neutral", 0),
+            new ContentTypeReader("PolygonHead.Pipeline.RenderDeferredLiquidEffectReader, PolygonHead", 0),
+            new ContentTypeReader("PolygonHead.Pipeline.LavaEffectReader, PolygonHead, Version=1.0.0.0, Culture=neutral", 0),
+
+            // Magicka Readers
+            new ContentTypeReader("Magicka.ContentReaders.LevelModelReader, Magicka", 0),
+            new ContentTypeReader("Magicka.ContentReaders.CharacterTemplateReader, Magicka, Version=1.0.0.0, Culture=neutral", 0),
+            new ContentTypeReader("Magicka.ContentReaders.PhysicsEntityTemplateReader, Magicka", 0),
+        };
+    }
+}
---
+++ MagickaPUP/MagickaPUP/XnaClasses/Xna/Data/XnaContentTypeReaders.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.XnaClasses.Xna.Data
+{
+    public static class XnaContentTypeReaders
+    {
+        public static readonly ContentTypeReader[] ContentTypeReaders = {
+            new ContentTypeReader("Microsoft.Xna.Framework.Content.VertexDeclarationReader", 0),
+            new ContentTypeReader("Microsoft.Xna.Framework.Content.VertexBufferReader", 0),
+            new ContentTypeReader("Microsoft.Xna.Framework.Content.IndexBufferReader", 0),
+            new ContentTypeReader("Microsoft.Xna.Framework.Content.ModelReader", 0),
+            new ContentTypeReader("Microsoft.Xna.Framework.Content.StringReader", 0),
+            new ContentTypeReader("Microsoft.Xna.Framework.Content.ListReader`1[[Microsoft.Xna.Framework.Vector3, Microsoft.Xna.Framework, Version=3.1.0.0, Culture=neutral, PublicKeyToken=6d5c3888ef60e27d]]", 0),
+            new ContentTypeReader("Microsoft.Xna.Framework.Content.Vector3Reader", 0),
+            new ContentTypeReader("Microsoft.Xna.Framework.Content.Texture2DReader", 0),
+        };
+    }
+}

e9efa7676475228492011907bc056369b54ac22d
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-02-24 21:46:39 +0100

    Created constructor for LzxCompression with window size exception handling
    When the wrong LZX window size is used, an exception is thrown explaining the issue.

--- MagickaPUP/MagickaPUP/Utility/Compression/LzxCompressor.cs
+++ MagickaPUP/MagickaPUP/Utility/Compression/LzxCompressor.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MagickaPUP.Utility.Exceptions;
 
 namespace MagickaPUP.Utility.Compression
 {
     // TODO : Implement
     public class LzxCompressor
     {
+        public uint[] positionBase = null;
+        public byte[] extraBits = null;
+
+        public LzxCompressor(int windowSize)
+        {
+            uint windowSizeShifted = (uint)(1 << windowSize);
+            int posSlots;
+
+            if (windowSize < 15 || windowSize > 21)
+                throw new LzxException($"Unsupported Window Size! Window Size is {windowSize}, but must be in range [15, 21]");
+
+
+        }
     }
 }

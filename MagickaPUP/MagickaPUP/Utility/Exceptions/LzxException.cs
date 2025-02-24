8b537a5e6e49001c874ec718c7e4df8c469490ec
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-02-24 21:43:45 +0100

    Created an exception class for LZX related operations

---
+++ MagickaPUP/MagickaPUP/Utility/Exceptions/LzxException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.Utility.Exceptions
+{
+    public class LzxException : Exception
+    {
+        protected static readonly string EXCEPTION_MSG_BASE = "LZX Exception";
+        protected static readonly string EXCEPTION_MSG_BODY = "An error occurred while performing an LZX compression operation";
+
+        public LzxException()
+        : base($"{EXCEPTION_MSG_BASE} : {EXCEPTION_MSG_BODY}")
+        { }
+
+        public LzxException(string message)
+        : base($"{EXCEPTION_MSG_BASE} : {message}")
+        { }
+
+        public LzxException(string message, Exception inner)
+        : base($"{EXCEPTION_MSG_BASE} : {message}", inner)
+        { }
+    }
+}

0fd92e6fb2880f2d85f38ea338b9da1b28033f66
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-02 17:16:03 +0100

    Added structs for settings to pack and unpack data

--- MagickaPUP/MagickaPUP/Core/ContentManager.cs
+++ MagickaPUP/MagickaPUP/Core/ContentManager.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace MagickaPUP.Core
 {
-    public class ContentManager
+    public static class ContentManager
     {
+        public struct PackSettings
+        {
+            public string InputFileName;
+            public string OutputFileName;
+            public bool CompressOutput;
+            public int DebugLevel;
+        }
+
+        public struct UnpackSettings
+        {
+            public string InputFileName;
+            public string OutputFileName;
+            public int DebugLevel;
+            public bool ShouldIndent;
+        }
+
+        public static void PackContent(PackSettings settings)
+        { }
+
+        public static void UnpackContent(UnpackSettings settings)
+        { }
     }
 }

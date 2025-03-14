cf4386ea79fe30000673d6613ee8198d54d235b2
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-14 16:44:05 +0100

    Created classes for new import and export pipelines idea

---
+++ MagickaPUP/MagickaPUP/Core/Content/Pipeline/Export/ExportPipeline.cs
@@ -0,0 +1,19 @@
+﻿using MagickaPUP.XnaClasses.Xnb;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.Core.Content.Pipeline.Export
+{
+    public abstract class ExportPipeline
+    {
+        public ExportPipeline()
+        { }
+
+        public abstract void Export(string fileName, XnbFile data);
+        public abstract void Export(Stream stream, XnbFile data);
+    }
+}
---
+++ MagickaPUP/MagickaPUP/Core/Content/Pipeline/Import/ImportPipeline.cs
@@ -0,0 +1,19 @@
+﻿using MagickaPUP.XnaClasses.Xnb;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.Core.Content.Pipeline.Import
+{
+    public abstract class ImportPipeline
+    {
+        public ImportPipeline()
+        { }
+
+        public abstract XnbFile Import(string fileName);
+        public abstract XnbFile Import(Stream stream);
+    }
+}

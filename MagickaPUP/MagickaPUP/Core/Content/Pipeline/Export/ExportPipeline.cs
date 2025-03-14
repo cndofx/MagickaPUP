51a4a284e01aa02bb8976f132ff5954018a5f5f9
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-14 16:53:56 +0100

    Added explanatory comments about the pipeline's innerworkings

--- MagickaPUP/MagickaPUP/Core/Content/Pipeline/Export/ExportPipeline.cs
+++ MagickaPUP/MagickaPUP/Core/Content/Pipeline/Export/ExportPipeline.cs
@@ -8,6 +8,14 @@ using System.Threading.Tasks;
 
 namespace MagickaPUP.Core.Content.Pipeline.Export
 {
+    // The Export Pipeline for MagickaPUP is as follows:
+    // Internal / in-memory representation ---> External / in-file representation
+    // XnbFile instance (C# class) ---> File.whatever (file)
+
+    // Note that obviously, the in-file representation can also be located fully in memory (that's what the Stream impl is for, so that we can have both
+    // file stream and memory stream support out of the box) and be yet another intermediate in-memory representation that can be further processed by
+    // yet another pipeline class for more complex pipelining.
+
     public abstract class ExportPipeline
     {
         public ExportPipeline()
--- MagickaPUP/MagickaPUP/Core/Content/Pipeline/Import/ImportPipeline.cs
+++ MagickaPUP/MagickaPUP/Core/Content/Pipeline/Import/ImportPipeline.cs
@@ -8,6 +8,14 @@ using System.Threading.Tasks;
 
 namespace MagickaPUP.Core.Content.Pipeline.Import
 {
+    // The Import Pipeline for MagickaPUP is as follows:
+    // External / in-file representation ---> Internal / in-memory representation
+    // File.whatever (file) ---> XnbFile instance (C# class)
+
+    // Note that obviously, the in-file representation can also be located fully in memory (that's what the Stream impl is for, so that we can have both
+    // file stream and memory stream support out of the box) and be yet another intermediate in-memory representation that can be further processed by
+    // yet another pipeline class for more complex pipelining.
+
     public abstract class ImportPipeline
     {
         public ImportPipeline()

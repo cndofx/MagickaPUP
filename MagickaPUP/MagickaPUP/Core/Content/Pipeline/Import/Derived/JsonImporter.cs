1cacb39f43808d69c68654eee5a9f0d5bc26ef23
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-14 16:48:54 +0100

    Created classes for the importer pipelines

---
+++ MagickaPUP/MagickaPUP/Core/Content/Pipeline/Import/Derived/JsonImporter.cs
@@ -0,0 +1,23 @@
+﻿using MagickaPUP.XnaClasses.Xnb;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.Core.Content.Pipeline.Import.Derived
+{
+    public class JsonImporter : ImportPipeline
+    {
+        public override XnbFile Import(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override XnbFile Import(Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

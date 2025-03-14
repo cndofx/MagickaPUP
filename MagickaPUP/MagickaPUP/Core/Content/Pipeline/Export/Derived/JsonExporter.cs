e0296d8ac52d3182dc82fd86e33ad88bdca2808e
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-14 16:46:54 +0100

    Created classes for the planned and available export pipelines

---
+++ MagickaPUP/MagickaPUP/Core/Content/Pipeline/Export/Derived/JsonExporter.cs
@@ -0,0 +1,23 @@
+﻿using MagickaPUP.XnaClasses.Xnb;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.Core.Content.Pipeline.Export.Derived
+{
+    public class JsonExporter : ExportPipeline
+    {
+        public override void Export(string fileName, XnbFile data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Export(Stream stream, XnbFile data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

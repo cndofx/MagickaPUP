6669e0b696496d82d3c3f08f2a541fc2266bdb60
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-15 23:45:37 +0100

    Created IUnpackable interface

---
+++ MagickaPUP/MagickaPUP/Core/Content/Pipeline/IUnpackable.cs
@@ -0,0 +1,15 @@
+﻿using MagickaPUP.Core.Content.Pipeline.Export;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.Core.Content.Pipeline
+{
+    public interface IUnpackable
+    {
+        public abstract ExporterBase<IUnpackable> ExporterUnpack { get; }
+        public abstract ExporterBase<IUnpackable> ImporterUnpack { get; }
+    }
+}

a68a76f91e1857107148ec19d5006e2ebc855a78
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-15 23:45:51 +0100

    Created IPipelineObject interface

---
+++ MagickaPUP/MagickaPUP/Core/Content/Pipeline/IPipelineObject.cs
@@ -0,0 +1,14 @@
+﻿using MagickaPUP.Core.Content.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.Core.Content.Pipeline
+{
+    public interface IPipelineObject
+    {
+        public FileType GetFileType();
+    }
+}

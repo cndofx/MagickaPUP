8465a95d95572e6ea050abbd5a9fe40f2de2d50c
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-15 15:34:49 +0100

    Created ContentType enum for file content type identification
    Since mpup is going to start supporting multiple file types, the time has come to add proper input and output file type handling.

---
+++ MagickaPUP/MagickaPUP/Core/Content/Data/ContentType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.Core.Content.Data
+{
+    // This enum is a custom enum that is used by MagickaPUP's ContentProcessor class to determine what type of content to use when generating the import and
+    // export pipeline calls for each type of file and stream.
+    public enum ContentType
+    {
+        Auto = 0,
+        Unknown,
+        Xnb,
+        Json,
+        Image // TODO : Maybe choose what type of image? as in explicitly saying png, jpg, bmp, etc...
+    }
+}

f0ad2ca8cb67222256acd6aafbd7f5832bc8fed3
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-15 15:35:01 +0100

    Created simple layout for ContentProcessor class

---
+++ MagickaPUP/MagickaPUP/Core/Content/Processor/ContentProcessor.cs
@@ -0,0 +1,63 @@
+﻿using MagickaPUP.Core.Content.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.Core.Content.Processor
+{
+    public class ContentProcessor
+    {
+        public ContentProcessor()
+        { }
+
+        public void ProcessFromArgs(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Process(string inputFile, string outputFile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Process(string inputFile, ContentType inputType, string outputFile, ContentType outputType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Process(Stream inputStream, ContentType inputType, Stream outputStream, ContentType outputType)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region PrivateMethods
+
+        private string GetFileNameExtension(string fileName)
+        {
+            return Path.GetExtension(fileName).ToLower();
+        }
+
+        private ContentType GetContentType(string fileName)
+        {
+            string extension = GetFileNameExtension(fileName);
+            switch (extension)
+            {
+                default:
+                    return ContentType.Unknown;
+                case "xnb":
+                    return ContentType.Xnb;
+                case "json":
+                    return ContentType.Json;
+                case "png":
+                case "jpg:":
+                case "bmp":
+                    return ContentType.Image;
+            }
+        }
+
+        #endregion
+    }
+}

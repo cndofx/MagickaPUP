f9bd92e125bc7c2a122a92f879f194698249bbca
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-15 20:50:37 +0100

    Created static helper class to get extensions from file types

---
+++ MagickaPUP/MagickaPUP/Core/Content/Data/FileTypeExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.Core.Content.Data
+{
+    public static class FileTypeExtension
+    {
+        private static readonly Dictionary<FileType, string> defaultFileTypeExtensions = new()
+        {
+            { FileType.Xnb, "xnb" },
+            { FileType.Json, "json" },
+            { FileType.Image, "png" },
+        };
+
+        public static string GetExtension(FileType fileType)
+        {
+            if (defaultFileTypeExtensions.ContainsKey(fileType))
+                return defaultFileTypeExtensions[fileType];
+            return "";
+        }
+    }
+}

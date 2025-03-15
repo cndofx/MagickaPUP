daf9df0e82ef4fbd8f4cfef0201f7239f2b0cb1a
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-15 20:52:14 +0100

    Added new Process overloads to ContentProcessor

--- MagickaPUP/MagickaPUP/Core/Content/Processor/ContentProcessor.cs
+++ MagickaPUP/MagickaPUP/Core/Content/Processor/ContentProcessor.cs
@@ -25,6 +25,7 @@ namespace MagickaPUP.Core.Content.Processor
 
         #region PublicMethods - Process
 
+        /*
         public void Process(string inputFile, string outputFile)
         {
             FileType inputContentType = GetContentType(inputFile);
@@ -53,6 +54,79 @@ namespace MagickaPUP.Core.Content.Processor
         public void Process(string fileName)
         {
             string extension = GetFileNameExtension(fileName);
+        }
+
+        public void Process(Stream inputStream, Stream outputStream, bool forceJsonOutput = false)
+        {
+            FileType inputType = FileType.Unknown;
+            FileType outputType = FileType.Unknown;
+
+            if (forceJsonOutput)
+                outputType = FileType.Json;
+
+            if(inputType == FileType.Unknown)
+                inputType = FileTypeDetector.GetFileType(inputStream);
+
+            var importer = GetImportPipeline(inputType);
+            var xnbFile = importer.Import(inputStream);
+
+            if(outputType == FileType.Unknown)
+                outputType = xnbFile.GetExporter().Export(outputStream, xnbFile)
+        }
+
+        public void XnbToJson(Stream inputStream, Stream outputStream)
+        {
+            XnbImporter importer = new XnbImporter();
+            JsonExporter exporter = new JsonExporter();
+
+            XnbFile xnbFile = importer.Import(inputStream);
+            exporter.Export(outputStream, xnbFile);
+        }
+        */
+
+        public void Process(Stream inputStream, Stream outputStream, ImportPipeline importer, ExportPipeline exporter)
+        {
+            XnbFile xnbFile = importer.Import(inputStream);
+            exporter.Export(outputStream, xnbFile);
+        }
+
+        public void Process(string inputFileName, string outputFileName, FileType inputType, FileType outputType)
+        {
+            ImportPipeline importer = GetImportPipeline(inputType);
+            ExportPipeline exporter = GetExportPipeline(outputType);
+
+            using (var inputStream = new FileStream(inputFileName, FileMode.Open, FileAccess.Read))
+            using (var outputStream = new FileStream(outputFileName, FileMode.Create, FileAccess.Write))
+            {
+                Process(inputStream, outputStream, importer, exporter);
+            }
+        }
+
+        public void Process(string inputFileName, string outputFileName)
+        {
+            FileType inputType = FileTypeDetector.GetFileType(inputFileName);
+            FileType outputType = FileTypeDetector.GetFileType(outputFileName);
+
+            Process(inputFileName, outputFileName, inputType, outputType);
+        }
+
+        public void Process(string inputFileName)
+        {
+            FileType inputType = FileTypeDetector.GetFileType(inputFileName);
+
+            // TODO : Get rid of this, implement system where we get the file type from the input stream after getting the xnb data, otherwise we just cannot know
+            // for sure whether we want a json or a png or whatever the fuck...
+
+            switch (inputType)
+            {
+                
+            }
+
+            FileType outputType = FileTypeDetector.GetFileType(outputFileName);
+
+            string outputFileName = Path.GetFileName(inputFileName) + "." + FileTypeExtension.GetExtension(outputType);
+
+            Process(inputFileName, outputFileName, inputType, outputType);
         }
 
         #endregion

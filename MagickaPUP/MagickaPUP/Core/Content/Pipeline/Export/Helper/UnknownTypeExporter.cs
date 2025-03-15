a45a54794f34770f68e04cc9d53b46449746e961
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-15 23:47:35 +0100

    Created auxiliary classes for unknown type importers

---
+++ MagickaPUP/MagickaPUP/Core/Content/Pipeline/Export/Helper/UnknownTypeExporter.cs
@@ -0,0 +1,25 @@
+﻿using MagickaPUP.XnaClasses.Xnb;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.Core.Content.Pipeline.Export.Helper
+{
+    /*
+    public class UnknownTypeExporter : ExportPipeline
+    {
+        public override void Export(string fileName, XnbFile data)
+        {
+            throw new Exception("The selected file type is unknown and cannot be exported!");
+        }
+
+        public override void Export(Stream stream, XnbFile data)
+        {
+            throw new Exception("The selected file type is unknown and cannot be exported!");
+        }
+    }
+    */
+}
---
+++ MagickaPUP/MagickaPUP/Core/Content/Pipeline/Import/Helper/UnknownTypeImporter.cs
@@ -0,0 +1,25 @@
+﻿using MagickaPUP.XnaClasses.Xnb;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.Core.Content.Pipeline.Import.Helper
+{
+    /*
+    public class UnknownTypeImporter : ImportPipeline
+    {
+        public override XnbFile Import(string fileName)
+        {
+            throw new Exception("The selected file type is unknown and cannot be imported!");
+        }
+
+        public override XnbFile Import(Stream stream)
+        {
+            throw new Exception("The selected file type is unknown and cannot be imported!");
+        }
+    }
+    */
+}

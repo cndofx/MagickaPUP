6d548c03616b4c545aac7389206f15d999b6a86d
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-15 23:45:29 +0100

    Created IPackable interface

---
+++ MagickaPUP/MagickaPUP/Core/Content/Pipeline/IPackable.cs
@@ -0,0 +1,15 @@
+﻿using MagickaPUP.Core.Content.Pipeline.Export;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.Core.Content.Pipeline
+{
+    public interface IPackable
+    {
+        public abstract ExporterBase<IPackable> ExporterPack { get; }
+        public abstract ExporterBase<IPackable> ImporterPack { get; }
+    }
+}

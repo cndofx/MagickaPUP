ea27d79e8793a9b1f503d72d4291d36d6996b472
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-16 04:33:48 +0100

    Created DxtCommon class to contain common functionality for all DXT decompression methods

---
+++ MagickaPUP/MagickaPUP/Utility/Compression/Dxt/DxtCommon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.Utility.Compression.Dxt
+{
+    public static class DxtUtil
+    {
+        public static void ConvertRgb565ToRgb888(ushort color, out byte r, out byte g, out byte b)
+        {
+            int temp;
+            temp = (color >> 11) * 255 + 16;
+            r = (byte)((temp / 32 + temp) / 32);
+            temp = ((color & 0x07E0) >> 5) * 255 + 32;
+            g = (byte)((temp / 64 + temp) / 64);
+            temp = (color & 0x001F) * 255 + 16;
+            b = (byte)((temp / 32 + temp) / 32);
+        }
+    }
+}

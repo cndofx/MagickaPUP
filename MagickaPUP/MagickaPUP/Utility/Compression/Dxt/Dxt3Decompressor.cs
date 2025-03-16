8bb6fdb999042db4f5dccabc3f40bcbce73566b5
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-16 04:39:09 +0100

    Created class for DXT3 decompression

---
+++ MagickaPUP/MagickaPUP/Utility/Compression/Dxt/Dxt3Decompressor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.Utility.Compression.Dxt
+{
+    public static class Dxt2Decompressor
+    {
+
+    }
+}

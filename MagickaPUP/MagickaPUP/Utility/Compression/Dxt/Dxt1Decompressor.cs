d5d45163042da47312c26daf0f083d124110b98e
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-16 04:34:00 +0100

    Implemented methods for DXT1 decompression

--- MagickaPUP/MagickaPUP/Utility/Compression/Dxt/Dxt1Decompressor.cs
+++ MagickaPUP/MagickaPUP/Utility/Compression/Dxt/Dxt1Decompressor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,118 @@ namespace MagickaPUP.Utility.Compression.Dxt
         public Dxt1Decompressor()
         { }
 
+        internal static byte[] DecompressDxt1(byte[] imageData, int width, int height)
+        {
+            using (MemoryStream imageStream = new MemoryStream(imageData))
+                return DecompressDxt1(imageStream, width, height);
+        }
 
+        internal static byte[] DecompressDxt1(Stream imageStream, int width, int height)
+        {
+            byte[] imageData = new byte[width * height * 4];
+
+            using (BinaryReader imageReader = new BinaryReader(imageStream))
+            {
+                int blockCountX = (width + 3) / 4;
+                int blockCountY = (height + 3) / 4;
+
+                for (int y = 0; y < blockCountY; y++)
+                {
+                    for (int x = 0; x < blockCountX; x++)
+                    {
+                        DecompressDxt1Block(imageReader, x, y, blockCountX, width, height, imageData);
+                    }
+                }
+            }
+
+            return imageData;
+        }
+
+        private static void DecompressDxt1Block(BinaryReader imageReader, int x, int y, int blockCountX, int width, int height, byte[] imageData)
+        {
+            ushort c0 = imageReader.ReadUInt16();
+            ushort c1 = imageReader.ReadUInt16();
+
+            byte r0, g0, b0;
+            byte r1, g1, b1;
+            DxtCommon.ConvertRgb565ToRgb888(c0, out r0, out g0, out b0);
+            DxtCommon.ConvertRgb565ToRgb888(c1, out r1, out g1, out b1);
+
+            uint lookupTable = imageReader.ReadUInt32();
+
+            for (int blockY = 0; blockY < 4; blockY++)
+            {
+                for (int blockX = 0; blockX < 4; blockX++)
+                {
+                    byte r = 0, g = 0, b = 0, a = 255;
+                    uint index = (lookupTable >> 2 * (4 * blockY + blockX)) & 0x03;
+
+                    if (c0 > c1)
+                    {
+                        switch (index)
+                        {
+                            case 0:
+                                r = r0;
+                                g = g0;
+                                b = b0;
+                                break;
+                            case 1:
+                                r = r1;
+                                g = g1;
+                                b = b1;
+                                break;
+                            case 2:
+                                r = (byte)((2 * r0 + r1) / 3);
+                                g = (byte)((2 * g0 + g1) / 3);
+                                b = (byte)((2 * b0 + b1) / 3);
+                                break;
+                            case 3:
+                                r = (byte)((r0 + 2 * r1) / 3);
+                                g = (byte)((g0 + 2 * g1) / 3);
+                                b = (byte)((b0 + 2 * b1) / 3);
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        switch (index)
+                        {
+                            case 0:
+                                r = r0;
+                                g = g0;
+                                b = b0;
+                                break;
+                            case 1:
+                                r = r1;
+                                g = g1;
+                                b = b1;
+                                break;
+                            case 2:
+                                r = (byte)((r0 + r1) / 2);
+                                g = (byte)((g0 + g1) / 2);
+                                b = (byte)((b0 + b1) / 2);
+                                break;
+                            case 3:
+                                r = 0;
+                                g = 0;
+                                b = 0;
+                                a = 0;
+                                break;
+                        }
+                    }
+
+                    int px = (x << 2) + blockX;
+                    int py = (y << 2) + blockY;
+                    if ((px < width) && (py < height))
+                    {
+                        int offset = ((py * width) + px) << 2;
+                        imageData[offset] = r;
+                        imageData[offset + 1] = g;
+                        imageData[offset + 2] = b;
+                        imageData[offset + 3] = a;
+                    }
+                }
+            }
+        }
     }
 }

90c36cb35352a847e33d5a114b739e7902e28498
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-22 19:53:31 +0100

    Renamed XnaInfo to XnaData

--- MagickaPUP/MagickaPUP/XnaClasses/Xna/Data/XnaData.cs
+++ MagickaPUP/MagickaPUP/XnaClasses/Xna/Data/XnaData.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MagickaPUP.XnaClasses.Xna.Data
 {
-    public static class XnaInfo
+    public static class XnaData
     {
         public enum ContentTypeReaderIndex
         {

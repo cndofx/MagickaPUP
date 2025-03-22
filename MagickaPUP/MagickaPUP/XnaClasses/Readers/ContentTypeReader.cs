eedca2e826dc5ada37d5d2d8f1421df7d1a95eb1
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-22 23:37:26 +0100

    Implemented functions to read and write the new content type reader struct

--- MagickaPUP/MagickaPUP/XnaClasses/Readers/ContentTypeReader.cs
+++ MagickaPUP/MagickaPUP/XnaClasses/Readers/ContentTypeReader.cs
@@ -17,5 +17,33 @@ namespace MagickaPUP.XnaClasses.Readers
             this.Name = name;
             this.Version = version;
         }
+
+        public ContentTypeReader(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading ContentTypeReader..."); // TODO : Maybe get rid of this? The other logging is enough, I think...
+
+            this.Name = reader.ReadString();
+            this.Version = reader.ReadInt32();
+
+            Log(logger);
+        }
+
+        public void Write(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing ContentTypeReader..."); // TODO : Maybe get rid of this? The other logging is enough, I think...
+
+            writer.Write(this.Name);
+            writer.Write(this.Version);
+
+            Log(logger);
+        }
+
+        private void Log(DebugLogger logger = null)
+        {
+            // NOTE : This is only encapsulated here because the message is identical on both places, once variable logging is standardised on the logger, we can get
+            // rid of this function and clean things up...
+            // TODO : Change logging level to something more adequate later on, maybe?
+            logger?.Log(2, $"ContentTypeReader : {{\"Name\" : \"{this.Name}\", \"Version\" : {this.Version}}}");
+        }
     }
 }

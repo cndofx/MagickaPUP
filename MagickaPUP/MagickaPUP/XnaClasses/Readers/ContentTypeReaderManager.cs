221e1624f37976c337fc1943e5162f5c7d3328fe
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-22 23:18:23 +0100

    Created ContentTypeReaderManager class

---
+++ MagickaPUP/MagickaPUP/XnaClasses/Readers/ContentTypeReaderManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.XnaClasses.Readers
+{
+    public class ContentTypeReaderManager
+    {
+        public Dictionary<ContentTypeReader, object> contentTypeReaders = new() {
+            { new ContentTypeReader(), new XnaObject() },
+        };
+
+
+    }
+}

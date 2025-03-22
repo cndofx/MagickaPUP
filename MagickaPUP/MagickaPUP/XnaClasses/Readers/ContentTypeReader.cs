3185cddbe306ec38dcb4cbae198b1e01d2d7a43d
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-22 23:18:06 +0100

    Created new ContentTypeReader struct

---
+++ MagickaPUP/MagickaPUP/XnaClasses/Readers/ContentTypeReader.cs
@@ -0,0 +1,21 @@
+﻿using MagickaPUP.Utility.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.XnaClasses.Readers
+{
+    public struct ContentTypeReader
+    {
+        public string Name { get; set; }
+        public int Version { get; set; }
+
+        public ContentTypeReader(string name = "", int version = 0)
+        {
+            this.Name = name;
+            this.Version = version;
+        }
+    }
+}

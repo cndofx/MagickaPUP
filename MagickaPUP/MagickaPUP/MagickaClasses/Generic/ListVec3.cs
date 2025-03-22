c746d2bb67d9a1a39a893349c628113d12df9c5e
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-22 19:17:20 +0100

    Created ListVec3 class
    This is literally just here to work around the issues with C# not allowing me to have static virtual methods or actual templates, so I need to work around this bullshit by creating something that is ugly and terrible...

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Generic/ListVec3.cs
@@ -0,0 +1,24 @@
+﻿using MagickaPUP.XnaClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Generic
+{
+    public class ListVec3 : XnaObject
+    {
+        #region Variables
+
+        #endregion
+
+        #region Constructor
+
+        #endregion
+
+        #region PublicMethods
+
+        #endregion
+    }
+}

3a1ebff9ac7a15e449a3ba81b0b09742ea1523e4
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-23 06:57:13 +0100

    Added content type reader entries to content type readers dictionary in readers manager

--- MagickaPUP/MagickaPUP/XnaClasses/Readers/ContentTypeReaderManager.cs
+++ MagickaPUP/MagickaPUP/XnaClasses/Readers/ContentTypeReaderManager.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using MagickaPUP.MagickaClasses.Character;
+using MagickaPUP.MagickaClasses.Effects;
+using MagickaPUP.MagickaClasses.Map;
+using MagickaPUP.MagickaClasses.PhysicsEntities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +14,19 @@ namespace MagickaPUP.XnaClasses.Readers
     {
         public Dictionary<ContentTypeReader, object> contentTypeReaders = new() {
             { new ContentTypeReader(), new XnaObject() },
+            { new ContentTypeReader("Magicka.ContentReaders.CharacterTemplateReader, Magicka, Version=1.0.0.0, Culture=neutral", 0), new CharacterTemplate() },
+            { new ContentTypeReader("PolygonHead.Pipeline.AdditiveEffectReader, PolygonHead, Version=1.0.0.0, Culture=neutral", 0), new EffectAdditive() },
+            { new ContentTypeReader("PolygonHead.Pipeline.RenderDeferredEffectReader, PolygonHead, Version=1.0.0.0, Culture=neutral", 0), new EffectDeferred() },
+            { new ContentTypeReader("PolygonHead.Pipeline.RenderDeferredLiquidEffectReader, PolygonHead", 0), new EffectDeferredLiquid() },
+            { new ContentTypeReader("PolygonHead.Pipeline.LavaEffectReader, PolygonHead, Version=1.0.0.0, Culture=neutral", 0), new EffectLava() },
+            { new ContentTypeReader("PolygonHead.Pipeline.BiTreeModelReader, PolygonHead", 0), new BiTreeModel() },
+            { new ContentTypeReader("Magicka.ContentReaders.LevelModelReader, Magicka", 0), new LevelModel() },
+            { new ContentTypeReader("Magicka.ContentReaders.PhysicsEntityTemplateReader, Magicka", 0), new PhysicsEntityTemplate() },
+            { new ContentTypeReader("Microsoft.Xna.Framework.Content.IndexBufferReader", 0), new IndexBuffer() },
+            { new ContentTypeReader("Microsoft.Xna.Framework.Content.ModelReader", 0), new Model() },
+            { new ContentTypeReader("Microsoft.Xna.Framework.Content.Texture2DReader", 0), new Texture2D() },
+            { new ContentTypeReader("Microsoft.Xna.Framework.Content.VertexBufferReader", 0), new VertexBuffer() },
+            { new ContentTypeReader("Microsoft.Xna.Framework.Content.VertexDeclarationReader", 0), new VertexDeclaration() },
         };
 
         public object Get(string name, int version)

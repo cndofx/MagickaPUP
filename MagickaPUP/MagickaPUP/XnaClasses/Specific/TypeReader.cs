a33469e2a955fe252bd46be3ef31e0b26f821e20
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-23 07:11:56 +0100

    Created TypeReader class
    This may not be the best idea, but this class is the base class for the ones that will be in charge of implementing the read and write methods for each type.
    
    This will require a major rework of the logic where the reading and writing is moved from each specific object to readers.
    
    The ReadInstance() and WriteInstance() methods will be completely needless and removed after this change is performed, if it is successful.
    
    Also, this means that the ReadObject() and WriteObject() functions are most likely either going to be moved into an XNA Utility class or into the binary Writer and Reader classes.

---
+++ MagickaPUP/MagickaPUP/XnaClasses/Specific/TypeReader.cs
@@ -0,0 +1,17 @@
+﻿using MagickaPUP.Utility.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.XnaClasses.Specific
+{
+    // Base class dedicated toward implementing the read and write methods for each class.
+    // Yes, the fucking name sucks since it does both reading and writing, but this is one of this things inherited from the nomenclature of the XNA framework...
+    public abstract class TypeReader<T>
+    {
+        public abstract void Write(MBinaryWriter writer, DebugLogger logger = null);
+        public abstract T Read(MBinaryReader reader, DebugLogger logger = null);
+    }
+}

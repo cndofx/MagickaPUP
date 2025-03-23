d5d17615b94a75f5fcfebd36a82acbd171e3271d
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-23 20:52:13 +0100

    Modified ContentTypeReaderManager to contain a dictionary with all new reader classes

--- MagickaPUP/MagickaPUP/XnaClasses/Readers/ContentTypeReaderManager.cs
+++ MagickaPUP/MagickaPUP/XnaClasses/Readers/ContentTypeReaderManager.cs
@@ -3,11 +3,10 @@ using MagickaPUP.MagickaClasses.Effects;
 using MagickaPUP.MagickaClasses.Generic;
 using MagickaPUP.MagickaClasses.Map;
 using MagickaPUP.MagickaClasses.PhysicsEntities;
+using MagickaPUP.XnaClasses.Specific;
+using MagickaPUP.XnaClasses.Specific.Derived;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MagickaPUP.XnaClasses.Readers
 {
@@ -15,34 +14,81 @@ namespace MagickaPUP.XnaClasses.Readers
     {
         // TODO : Replace all of these placeholder / dummy new() expressions with actual reader classes so that we can properly read the contents of each of these objects.
         public Dictionary<ContentTypeReader, object> contentTypeReaders = new() {
-            { new ContentTypeReader(), new XnaObject() }, // TODO : Get rid of this shit maybe?
-            { new ContentTypeReader("Magicka.ContentReaders.CharacterTemplateReader, Magicka, Version=1.0.0.0, Culture=neutral", 0), new CharacterTemplate() },
-            { new ContentTypeReader("PolygonHead.Pipeline.AdditiveEffectReader, PolygonHead, Version=1.0.0.0, Culture=neutral", 0), new EffectAdditive() },
-            { new ContentTypeReader("PolygonHead.Pipeline.RenderDeferredEffectReader, PolygonHead, Version=1.0.0.0, Culture=neutral", 0), new EffectDeferred() },
-            { new ContentTypeReader("PolygonHead.Pipeline.RenderDeferredLiquidEffectReader, PolygonHead", 0), new EffectDeferredLiquid() },
-            { new ContentTypeReader("PolygonHead.Pipeline.LavaEffectReader, PolygonHead, Version=1.0.0.0, Culture=neutral", 0), new EffectLava() },
-            { new ContentTypeReader("PolygonHead.Pipeline.BiTreeModelReader, PolygonHead", 0), new BiTreeModel() },
-            { new ContentTypeReader("Magicka.ContentReaders.LevelModelReader, Magicka", 0), new LevelModel() },
-            { new ContentTypeReader("Magicka.ContentReaders.PhysicsEntityTemplateReader, Magicka", 0), new PhysicsEntityTemplate() },
-            { new ContentTypeReader("Microsoft.Xna.Framework.Content.IndexBufferReader", 0), new IndexBuffer() },
-            { new ContentTypeReader("Microsoft.Xna.Framework.Content.ModelReader", 0), new Model() },
-            { new ContentTypeReader("Microsoft.Xna.Framework.Content.Texture2DReader", 0), new Texture2D() },
-            { new ContentTypeReader("Microsoft.Xna.Framework.Content.VertexBufferReader", 0), new VertexBuffer() },
-            { new ContentTypeReader("Microsoft.Xna.Framework.Content.VertexDeclarationReader", 0), new VertexDeclaration() },
-            { new ContentTypeReader("Microsoft.Xna.Framework.Content.StringReader", 0), default(string) }, // TODO : Implement a reader class...
-            { new ContentTypeReader("Microsoft.Xna.Framework.Content.ListReader`1[[Microsoft.Xna.Framework.Vector3, Microsoft.Xna.Framework, Version=3.1.0.0, Culture=neutral, PublicKeyToken=6d5c3888ef60e27d]]", 0), new List<Vec3>() }, // TODO : Implement reader class for generic list reading and for specific List<T> reading, such as the list of vec3...
-            { new ContentTypeReader("Microsoft.Xna.Framework.Content.Vector3Reader", 0), new Vec3() },
+            {
+                new ContentTypeReader("Magicka.ContentReaders.CharacterTemplateReader, Magicka, Version=1.0.0.0, Culture=neutral", 0),
+                new CharacterTemplateReader()
+            },
+            {
+                new ContentTypeReader("PolygonHead.Pipeline.AdditiveEffectReader, PolygonHead, Version=1.0.0.0, Culture=neutral", 0),
+                new EffectAdditiveReader()
+            },
+            {
+                new ContentTypeReader("PolygonHead.Pipeline.RenderDeferredEffectReader, PolygonHead, Version=1.0.0.0, Culture=neutral", 0),
+                new EffectDeferredReader()
+            },
+            {
+                new ContentTypeReader("PolygonHead.Pipeline.RenderDeferredLiquidEffectReader, PolygonHead", 0),
+                new EffectDeferredLiquidReader()
+            },
+            {
+                new ContentTypeReader("PolygonHead.Pipeline.LavaEffectReader, PolygonHead, Version=1.0.0.0, Culture=neutral", 0),
+                new EffectLavaReader()
+            },
+            {
+                new ContentTypeReader("PolygonHead.Pipeline.BiTreeModelReader, PolygonHead", 0),
+                new BiTreeModelReader()
+            },
+            {
+                new ContentTypeReader("Magicka.ContentReaders.LevelModelReader, Magicka", 0),
+                new LevelModelReader()
+            },
+            {
+                new ContentTypeReader("Magicka.ContentReaders.PhysicsEntityTemplateReader, Magicka", 0),
+                new PhysicsEntityTemplateReader()
+            },
+            {
+                new ContentTypeReader("Microsoft.Xna.Framework.Content.IndexBufferReader", 0),
+                new IndexBufferReader()
+            },
+            {
+                new ContentTypeReader("Microsoft.Xna.Framework.Content.ModelReader", 0),
+                new ModelReader()
+            },
+            {
+                new ContentTypeReader("Microsoft.Xna.Framework.Content.Texture2DReader", 0),
+                new Texture2DReader()
+            },
+            {
+                new ContentTypeReader("Microsoft.Xna.Framework.Content.VertexBufferReader", 0),
+                new VertexBufferReader()
+            },
+            {
+                new ContentTypeReader("Microsoft.Xna.Framework.Content.VertexDeclarationReader", 0),
+                new VertexDeclarationReader()
+            },
+            {
+                new ContentTypeReader("Microsoft.Xna.Framework.Content.StringReader", 0),
+                new StringReader()
+            },
+            {
+                new ContentTypeReader("Microsoft.Xna.Framework.Content.ListReader`1[[Microsoft.Xna.Framework.Vector3, Microsoft.Xna.Framework, Version=3.1.0.0, Culture=neutral, PublicKeyToken=6d5c3888ef60e27d]]", 0),
+                new List<Vec3>() // TODO : Implement
+            },
+            {
+                new ContentTypeReader("Microsoft.Xna.Framework.Content.Vector3Reader", 0),
+                new Vector3Reader()
+            },
         };
 
-        public object Get(string name, int version)
+        public TypeReader<object> Get(string name, int version)
         {
             return Get(new ContentTypeReader(name, version));
         }
 
-        public object Get(ContentTypeReader contentType)
+        public TypeReader<object> Get(ContentTypeReader contentType)
         {
             if (this.contentTypeReaders.ContainsKey(contentType))
-                return this.contentTypeReaders[contentType];
+                return (TypeReader<object>)this.contentTypeReaders[contentType];
             throw new Exception($"The ContentTypeReader \"{contentType.Name}\" is not implemented yet!"); // If the requested content type reader does not exist within the dictionary, then we just say it is not supported yet and bail out.
             // return null;
         }

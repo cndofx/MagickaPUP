2f7720e945297faf12199fec542616e1bdfc36ec
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-23 07:15:12 +0100

    Added constructor to TypeReader

--- MagickaPUP/MagickaPUP/XnaClasses/Specific/TypeReader.cs
+++ MagickaPUP/MagickaPUP/XnaClasses/Specific/TypeReader.cs
@@ -9,9 +9,12 @@ namespace MagickaPUP.XnaClasses.Specific
 {
     // Base class dedicated toward implementing the read and write methods for each class.
     // Yes, the fucking name sucks since it does both reading and writing, but this is one of this things inherited from the nomenclature of the XNA framework...
+    // TODO : Maybe rename to "TypeHandler" or "TypeReaderWriter" ? or just separate the logic into 2 classes?
     public abstract class TypeReader<T>
     {
-        public abstract void Write(MBinaryWriter writer, DebugLogger logger = null);
+        public TypeReader()
+        { }
         public abstract T Read(MBinaryReader reader, DebugLogger logger = null);
+        public abstract void Write(MBinaryWriter writer, DebugLogger logger = null);
     }
 }

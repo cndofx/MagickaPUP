8c7ff11af3abfbbf8bacda413ecd933128e8d969
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-24 16:21:44 +0100

    Modified TypeReader<T> to inherit from TypeReaderBase

--- MagickaPUP/MagickaPUP/XnaClasses/ContentType/Readers/TypeReader.cs
+++ MagickaPUP/MagickaPUP/XnaClasses/ContentType/Readers/TypeReader.cs
@@ -1,4 +1,5 @@
 ﻿using MagickaPUP.Utility.IO;
+using MagickaPUP.XnaClasses.ContentType.Readers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +8,18 @@ using System.Threading.Tasks;
 
 namespace MagickaPUP.XnaClasses.Specific
 {
-    public abstract class TypeReader<T>
+    public abstract class TypeReader<T> : TypeReaderBase where T : class
     {
         public TypeReader()
         { }
 
-        public abstract T Read(MBinaryReader reader, DebugLogger logger = null);
+        public abstract T Read(T instance, MBinaryReader reader, DebugLogger logger = null);
+
+        public override object Read(object instance, MBinaryReader reader, DebugLogger logger = null)
+        {
+            if (instance == null)
+                instance = default(T);
+            return Read((T)instance, reader, logger);
+        }
     }
 }

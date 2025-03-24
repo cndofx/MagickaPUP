afa73f4073195e248ce65dc0e388b6f5e10ff890
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-24 16:28:34 +0100

    Added explanatory comment

--- MagickaPUP/MagickaPUP/XnaClasses/ContentType/Readers/TypeReader.cs
+++ MagickaPUP/MagickaPUP/XnaClasses/ContentType/Readers/TypeReader.cs
@@ -13,7 +13,7 @@ namespace MagickaPUP.XnaClasses.Specific
         public TypeReader()
         { }
 
-        public abstract T Read(T instance, MBinaryReader reader, DebugLogger logger = null);
+        public abstract T Read(T instance, MBinaryReader reader, DebugLogger logger = null); // NOTE : Even in XNA and Monogame, this instance parameter is mostly fucking useless... for the most part, this works as a way to trick the fucking stupid compiler into knowing what the correct method to call is. A workaround to avoid having to use this shit would be to simply remove it in this case, since the base Read() method is already implemented taking an object param, but whatever... fuck this shit, for now it's ok, I guess...
 
         public override object Read(object instance, MBinaryReader reader, DebugLogger logger = null)
         {

01259d58055a0ca8a2591d6dcac5c9684295f3e7
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-24 15:31:42 +0100

    Created TypeWriter class

---
+++ MagickaPUP/MagickaPUP/XnaClasses/ContentType/Writers/TypeWriter.cs
@@ -0,0 +1,17 @@
+﻿using MagickaPUP.Utility.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.XnaClasses.ContentType.Writers
+{
+    public abstract class TypeWriter<T>
+    {
+        public TypeWriter()
+        { }
+        
+        public abstract void Write(T instance, MBinaryWriter writer, DebugLogger logger = null);
+    }
+}

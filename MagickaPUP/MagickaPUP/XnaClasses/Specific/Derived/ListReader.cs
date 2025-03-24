1a00877539c102f467c1299f2275b1c3420b6fda
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-24 02:10:04 +0100

    Created ListReader class
    TODO : Implement

---
+++ MagickaPUP/MagickaPUP/XnaClasses/Specific/Derived/ListReader.cs
@@ -0,0 +1,25 @@
+﻿using MagickaPUP.Utility.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.XnaClasses.Specific.Derived
+{
+    public class ListReader<T> : TypeReader<List<T>>
+    {
+        public ListReader()
+        { }
+
+        public override List<T> Read(MBinaryReader reader, DebugLogger logger = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(List<T> instance, MBinaryWriter writer, DebugLogger logger = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

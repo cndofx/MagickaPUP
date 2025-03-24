b1e3f1e7e52ad075be4db61877a5a22b4802510f
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-24 12:46:58 +0100

    Created XnaUtility static class

---
+++ MagickaPUP/MagickaPUP/XnaClasses/Xna/XnaUtility.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.XnaClasses.Xna
+{
+    public static class XnaUtility
+    {
+
+    }
+}

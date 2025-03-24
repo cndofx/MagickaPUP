fcc7fb13a2b4bbf201835d5f42fd070520e22550
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-24 02:17:14 +0100

    Added experimental type writer classes

---
+++ MagickaPUP/MagickaPUP/XnaClasses/Writers/ContentTypeWriter.cs
@@ -0,0 +1,23 @@
+﻿using MagickaPUP.Utility.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.XnaClasses.Writers
+{
+    public class ContentWriter<T> where T : XnaObject, new()
+    {
+        public ContentWriter()
+        { }
+
+        public string Name { get; set; }
+        public int Version { get; set; }
+
+        public virtual void Write(T t, MBinaryWriter writer, DebugLogger logger = null)
+        {
+            t.WriteInstance(writer, logger);
+        }
+    }
+}

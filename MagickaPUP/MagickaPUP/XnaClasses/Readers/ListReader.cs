4e10f3ad4064e37b856e347c588311008d02f633
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-24 02:16:00 +0100

    Added experimental list reader test class

---
+++ MagickaPUP/MagickaPUP/XnaClasses/Readers/ListReader.cs
@@ -0,0 +1,16 @@
+﻿using MagickaPUP.MagickaClasses.Liquids;
+using MagickaPUP.Utility.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.XnaClasses.Readers
+{
+    public class ListReader<T>
+    {
+        public void Read()
+        { }
+    }
+}

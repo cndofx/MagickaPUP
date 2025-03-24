5d8db9680c602d85202e55a3f2ea3cf50d4cadcc
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-24 16:35:25 +0100

    Modified TypeWriter<T> to inherit from TypeWriterBase

--- MagickaPUP/MagickaPUP/XnaClasses/ContentType/Writers/TypeWriter.cs
+++ MagickaPUP/MagickaPUP/XnaClasses/ContentType/Writers/TypeWriter.cs
@@ -7,11 +7,16 @@ using System.Threading.Tasks;
 
 namespace MagickaPUP.XnaClasses.ContentType.Writers
 {
-    public abstract class TypeWriter<T>
+    public abstract class TypeWriter<T> : TypeWriterBase where T : class
     {
         public TypeWriter()
         { }
         
         public abstract void Write(T instance, MBinaryWriter writer, DebugLogger logger = null);
+
+        public override void Write(object instance, MBinaryWriter writer, DebugLogger logger = null)
+        {
+            Write((T)instance, writer, logger);
+        }
     }
 }

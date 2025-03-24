7f91993f9e4a74a4c4c0e4819a839edfa22e478e
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-24 16:35:10 +0100

    Created TypeWriterBase class

---
+++ MagickaPUP/MagickaPUP/XnaClasses/ContentType/Writers/TypeWriterBase.cs
@@ -0,0 +1,17 @@
+﻿using MagickaPUP.Utility.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.XnaClasses.ContentType.Writers
+{
+    public abstract class TypeWriterBase
+    {
+        public TypeWriterBase()
+        { }
+
+        public abstract void Write(object instance, MBinaryWriter writer, DebugLogger logger = null);
+    }
+}

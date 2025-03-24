26cb91dcddd24c3d3de64dd0e72ec3bab0da8218
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-24 12:52:17 +0100

    Implemented logic for ReadObject method to read XNB reader index and null object handling

--- MagickaPUP/MagickaPUP/XnaClasses/Xna/XnaUtility.cs
+++ MagickaPUP/MagickaPUP/XnaClasses/Xna/XnaUtility.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MagickaPUP.Utility.IO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,20 @@ namespace MagickaPUP.XnaClasses.Xna
 {
     public static class XnaUtility
     {
+        public static T ReadObject<T>(MBinaryReader reader, DebugLogger logger = null) where T : class
+        {
+            T ans = default(T);
 
+            int indexXnb = reader.Read7BitEncodedInt();
+            int indexMem = indexXnb - 1;
+
+            if (indexXnb == 0)
+            {
+                logger?.Log(1, "Reading NULL Object");
+                return null;
+            }
+
+            return ans;
+        }
     }
 }

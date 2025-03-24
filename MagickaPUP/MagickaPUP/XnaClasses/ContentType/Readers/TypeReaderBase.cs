e7eb9ef76d578683dd2a9ea7595bc1a6faa4eb7e
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-24 16:21:32 +0100

    Created TypeReaderBase class

---
+++ MagickaPUP/MagickaPUP/XnaClasses/ContentType/Readers/TypeReaderBase.cs
@@ -0,0 +1,17 @@
+﻿using MagickaPUP.Utility.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.XnaClasses.ContentType.Readers
+{
+    public abstract class TypeReaderBase
+    {
+        public TypeReaderBase()
+        { }
+
+        public abstract object Read(object instance, MBinaryReader reader, DebugLogger logger = null);
+    }
+}

227735f6fc094606bd8cce7ccd66ee18ee9e680c
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-25 13:35:56 +0100

    Removed outdated comment

--- MagickaPUP/MagickaPUP/XnaClasses/ContentType/Readers/Derived/ListReader.cs
+++ MagickaPUP/MagickaPUP/XnaClasses/ContentType/Readers/Derived/ListReader.cs
@@ -8,10 +8,6 @@ using System.Threading.Tasks;
 
 namespace MagickaPUP.XnaClasses.Specific.Derived
 {
-    // TODO : Implement
-    // NOTE : Both the Read and Write methods should call in the future the WriteObject and ReadObject functions from the reader and writer.
-    // Yes, that means that we need to move the logic for object writing and reading from XnaObject into the binary reader and writer classes.
-    // And probably also rename those classes to ContentReader and ContentWriter or whatever the fuck...
     public class ListReader<T, TReader> : TypeReader<List<T>> where T : class where TReader : TypeReaderBase, new()
     {
         public ListReader()

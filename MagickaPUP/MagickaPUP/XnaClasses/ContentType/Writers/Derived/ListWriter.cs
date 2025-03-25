e3d68e3b5c2fc49ee7d8a63e08333f2e893c77f0
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-25 13:38:47 +0100

    Implemented writer logic for ListWriter<T>

--- MagickaPUP/MagickaPUP/XnaClasses/ContentType/Writers/Derived/ListWriter.cs
+++ MagickaPUP/MagickaPUP/XnaClasses/ContentType/Writers/Derived/ListWriter.cs
@@ -1,25 +1,27 @@
 ﻿using MagickaPUP.Utility.IO;
+using MagickaPUP.XnaClasses.ContentType.Readers;
 using MagickaPUP.XnaClasses.ContentType.Writers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MagickaPUP.XnaClasses.Specific.Derived
 {
-    // TODO : Implement
-    // NOTE : Both the Read and Write methods should call in the future the WriteObject and ReadObject functions from the reader and writer.
-    // Yes, that means that we need to move the logic for object writing and reading from XnaObject into the binary reader and writer classes.
-    // And probably also rename those classes to ContentReader and ContentWriter or whatever the fuck...
-    public class ListWriter<T> : TypeWriter<List<T>>
+    public class ListWriter<T, TWriter> : TypeWriter<List<T>> where T : class where TWriter : TypeWriterBase, new()
     {
         public ListWriter()
         { }
 
         public override void Write(List<T> instance, MBinaryWriter writer, DebugLogger logger = null)
         {
-            throw new NotImplementedException();
+            TypeWriterBase elementWriter = new TWriter();
+            for (int i = 0; i < instance.Count; ++i)
+            {
+                elementWriter.Write(instance[i], writer, logger);
+            }
         }
     }
 }

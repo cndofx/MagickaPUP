5b70860a6a4ddea09a8aeca7947129cd030c2c6e
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-26 13:35:35 +0100

    Moved XNA Version logic into XnaData static class

--- MagickaPUP/MagickaPUP/XnaClasses/Xna/Data/XnaData.cs
+++ MagickaPUP/MagickaPUP/XnaClasses/Xna/Data/XnaData.cs
@@ -8,6 +8,28 @@ namespace MagickaPUP.XnaClasses.Xna.Data
 {
     public static class XnaData
     {
+        public enum XnaVersion : byte
+        {
+            Version_3_1 = 4,
+            Version_4 = 5
+        }
 
+        public static string XnaVersionString(XnaVersion version)
+        {
+            switch (version)
+            {
+                default:
+                    return "XNA Version Unkown";
+                case XnaVersion.Version_3_1:
+                    return "XNA Version 3.1";
+                case XnaVersion.Version_4:
+                    return "XNA Version 4.0";
+            }
+        }
+
+        public static byte XnaVersionByte(XnaVersion version)
+        {
+            return (byte)(version);
+        }
     }
 }

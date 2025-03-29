e2b678693e781e63e85f76235c297f80122143b3
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-29 20:02:57 +0100

    Created abstract class for SpecialAbility logic
    Note that Special Abilities are not to be confused with "regular" Abilities.

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Item/SpecialAbilities/SpecialAbility.cs
@@ -0,0 +1,17 @@
+﻿using MagickaPUP.Utility.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Item.SpecialAbilities
+{
+    public abstract class SpecialAbility
+    {
+        public SpecialAbility()
+        { }
+
+        public abstract void Write(MBinaryWriter writer, DebugLogger logger = null);
+    }
+}

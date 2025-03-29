69b0374311f2ee38c1acd666a633995d972f9c23
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-29 16:47:59 +0100

    Created PassiveAbilityType byte enum

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Data/PassiveAbilityType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Data
+{
+    // NOTE : This is named "PassiveAbilities" within Magicka's code.
+    public enum PassiveAbilityType : byte
+    {
+        None,
+        ShieldBoost,
+        AreaLifeDrain,
+        ZombieDeterrent,
+        ReduceAggro,
+        EnhanceAllyMelee,
+        AreaRegeneration,
+        InverseArcaneLife,
+        Zap,
+        BirchSteam,
+        WetLightning,
+        MoveSpeed,
+        Glow,
+        Mjolnr,
+        Gungner,
+        MasterSword,
+        DragonSlayer,
+        COUNT
+    }
+}

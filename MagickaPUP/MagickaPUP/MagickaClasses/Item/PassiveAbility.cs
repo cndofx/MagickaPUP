3f48175c0258005022bce46f013c196eb9da3351
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-29 16:48:09 +0100

    Created PassiveAbility struct

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Item/PassiveAbility.cs
@@ -0,0 +1,39 @@
+﻿using MagickaPUP.MagickaClasses.Data;
+using MagickaPUP.Utility.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Item
+{
+    public struct PassiveAbility
+    {
+
+        public PassiveAbilityType Ability { get; set; }
+        public float Variable { get; set; } // TODO : Figure out what the fuck this thing does... but by the looks of it, it is probably the magnitude of the ability, or something like that...
+
+        public PassiveAbility(PassiveAbilityType ability = PassiveAbilityType.None, float variable = 0.0f)
+        {
+            this.Ability = ability;
+            this.Variable = variable;
+        }
+
+        public PassiveAbility(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading Passive Ability...");
+
+            this.Ability = (PassiveAbilityType)reader.ReadByte();
+            this.Variable = reader.ReadSingle();
+        }
+
+        public void Write(MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing Passive Ability...");
+
+            writer.Write((byte)this.Ability);
+            writer.Write(this.Variable);
+        }
+    }
+}

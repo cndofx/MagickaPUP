a47364f5f27fd41e2c8f1954e6e2dd7438620a84
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-29 15:18:42 +0100

    Added WeaponClass enum
    This enum offers a byte that is used to determine the class / type of an item.

---
+++ MagickaPUP/MagickaPUP/MagickaClasses/Data/WeaponClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.MagickaClasses.Data
+{
+    public enum WeaponClass : byte
+    {
+        Default,
+        Thrust_Fast,
+        Thrust_Medium,
+        Thrust_Slow,
+        Crush_Fast,
+        Crush_Medium,
+        Crush_Slow,
+        Slash_Fast,
+        Slash_Medium,
+        Slash_Slow,
+        Throw_Fast,
+        Throw_Medium,
+        Throw_Slow,
+        Unarmed,
+        Handgun,
+        Rifle,
+        Machinegun,
+        Heavy,
+        Staff,
+        NewAnimationSet0,
+        NewAnimationSet1,
+        NewAnimationSet2,
+        NewAnimationSet3,
+        NewAnimationSet4,
+        NewAnimationSet5,
+        NewAnimationSet6,
+        NewAnimationSet7,
+        COUNT
+    }
+}

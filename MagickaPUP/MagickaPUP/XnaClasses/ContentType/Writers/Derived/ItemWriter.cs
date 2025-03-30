802133166ee48447656c1f4379296b651513be1d
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-30 15:50:58 +0200

    Created ItemWriter type writer class

---
+++ MagickaPUP/MagickaPUP/XnaClasses/ContentType/Writers/Derived/ItemWriter.cs
@@ -0,0 +1,22 @@
+﻿using MagickaPUP.MagickaClasses.Item;
+using MagickaPUP.Utility.IO;
+using MagickaPUP.XnaClasses.Specific;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.XnaClasses.ContentType.Writers.Derived
+{
+    public class ItemWriter : TypeWriter<Item>
+    {
+        public ItemWriter()
+        { }
+
+        public override void Write(Item instance, MBinaryWriter writer, DebugLogger logger = null)
+        {
+            Item.Write(instance, writer, logger);
+        }
+    }
+}

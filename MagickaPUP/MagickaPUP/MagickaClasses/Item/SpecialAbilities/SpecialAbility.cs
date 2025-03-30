885c4cfaab7a3601ae09a20eacb2dc4f28623127
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-30 16:36:42 +0200

    Implemented reading for SpecialAbility class

--- MagickaPUP/MagickaPUP/MagickaClasses/Item/SpecialAbilities/SpecialAbility.cs
+++ MagickaPUP/MagickaPUP/MagickaClasses/Item/SpecialAbilities/SpecialAbility.cs
@@ -1,4 +1,5 @@
-﻿using MagickaPUP.Utility.IO;
+﻿using MagickaPUP.MagickaClasses.Data;
+using MagickaPUP.Utility.IO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +8,39 @@ using System.Threading.Tasks;
 
 namespace MagickaPUP.MagickaClasses.Item.SpecialAbilities
 {
-    public abstract class SpecialAbility
+    public class SpecialAbility
     {
-        public SpecialAbility()
-        { }
+        public string Type { get; set; }
+        public string Animation { get; set; }
+        public string Hash { get; set; }
+        public Elements[] Elements { get; set; }
 
-        public abstract void Write(MBinaryWriter writer, DebugLogger logger = null);
+        public SpecialAbility()
+        {
+            this.Type = default;
+            this.Animation = default;
+            this.Hash = default;
+        }
+
+        public SpecialAbility(MBinaryReader reader, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Reading Special Ability...");
+
+            this.Type = reader.ReadString();
+            this.Animation = reader.ReadString();
+            this.Hash= reader.ReadString();
+
+            int numElements = reader.ReadInt32();
+            this.Elements = new Elements[numElements];
+            for(int i = 0; i < numElements; ++i)
+                this.Elements[i] = (Elements)reader.ReadInt32();
+        }
+
+        public static void Write(SpecialAbility instance, MBinaryWriter writer, DebugLogger logger = null)
+        {
+            logger?.Log(1, "Writing Special Ability...");
+
+            throw new NotImplementedException("Write SpecialAbility is not implemented yet!");
+        }
     }
 }

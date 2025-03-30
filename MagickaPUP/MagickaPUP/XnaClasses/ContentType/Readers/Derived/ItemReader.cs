a00ce9e743dd94ba83807d8dd8b31a295d1c90ac
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-30 15:50:50 +0200

    Created ItemReader type reader class

---
+++ MagickaPUP/MagickaPUP/XnaClasses/ContentType/Readers/Derived/ItemReader.cs
@@ -0,0 +1,22 @@
+﻿using MagickaPUP.MagickaClasses.Item;
+using MagickaPUP.Utility.IO;
+using MagickaPUP.XnaClasses.Specific;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.XnaClasses.ContentType.Readers.Derived
+{
+    public class ItemReader : TypeReader<Item>
+    {
+        public ItemReader()
+        { }
+
+        public override Item Read(Item instance, MBinaryReader reader, DebugLogger logger = null)
+        {
+            return Item.Read(reader, logger);
+        }
+    }
+}

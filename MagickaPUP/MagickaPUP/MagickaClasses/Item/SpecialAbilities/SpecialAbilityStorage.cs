c19763f2127388b62a84558329e6c12b3cd3db98
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-03-30 16:39:27 +0200

    Added special ability reading to special ability storage

--- MagickaPUP/MagickaPUP/MagickaClasses/Item/SpecialAbilities/SpecialAbilityStorage.cs
+++ MagickaPUP/MagickaPUP/MagickaClasses/Item/SpecialAbilities/SpecialAbilityStorage.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 namespace MagickaPUP.MagickaClasses.Item.SpecialAbilities
 {
     // NOTE : This class does NOT exist within Magicka's code. This is just a proxy class that was added to make it easier to structure the code.
+    // TODO : Get rid of this class, maybe? it's not really useful when compared to the analogue for regular abilities, since that actually does benefit from this due to its more complex derived class structure... this one just exists to bother us and that's it lol...
     public class SpecialAbilityStorage
     {
         public bool HasSpecialAbility { get; set; }
@@ -29,10 +30,13 @@ namespace MagickaPUP.MagickaClasses.Item.SpecialAbilities
             if (this.HasSpecialAbility)
             {
                 this.SpecialAbilityRechargeTime = reader.ReadSingle();
-                this.SpecialAbility = null; // TODO : Implement SpecialAbility reading...
+                this.SpecialAbility = new SpecialAbility(reader, logger);
             }
-
-            throw new NotImplementedException("Read SpecialAbility Data is not implemented yet!");
+            else
+            {
+                this.SpecialAbilityRechargeTime = 0.0f;
+                this.SpecialAbility = null;
+            }
         }
 
         public void Write(MBinaryWriter writer, DebugLogger logger = null)

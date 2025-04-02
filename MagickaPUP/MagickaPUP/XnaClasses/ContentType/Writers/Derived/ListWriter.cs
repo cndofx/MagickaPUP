8f2dbd2d846e52af193acc3def731e94561c5699
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-02 22:36:20 +0200

    Added shitty patch to include individual element type reader when writing content type reader for lists

--- MagickaPUP/MagickaPUP/XnaClasses/ContentType/Writers/Derived/ListWriter.cs
+++ MagickaPUP/MagickaPUP/XnaClasses/ContentType/Writers/Derived/ListWriter.cs
@@ -17,6 +17,14 @@ namespace MagickaPUP.XnaClasses.Specific.Derived
 
         public override void Write(List<T> instance, MBinaryWriter writer, DebugLogger logger = null)
         {
+            // XNA requires that the reader for base type T is also present, so we must ensure that the reader is added, even if we don't read individual objects.
+            // This is due to the fact that even structs use content type readers, so this behaviour will need to be streamlined in the future by improving the way
+            // that list reading and writing works on mpup.
+            var contentTypeReader = writer.ContentTypeReaderManager.GetContentTypeReader(typeof(T));
+            writer.ContentTypeReaderStorage.AddReader(contentTypeReader);
+
+
+            // Write the data of this type
             TypeWriterBase elementWriter = new TWriter();
             for (int i = 0; i < instance.Count; ++i)
             {

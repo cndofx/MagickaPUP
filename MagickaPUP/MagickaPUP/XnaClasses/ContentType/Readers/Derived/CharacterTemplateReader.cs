496e18e13cc9df4197382bd40ac036673909ce36
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-05 15:48:36 +0200

    Implemented partial support for CharacterTemplateReader to read old, new and auto magicka game versions

--- MagickaPUP/MagickaPUP/XnaClasses/ContentType/Readers/Derived/CharacterTemplateReader.cs
+++ MagickaPUP/MagickaPUP/XnaClasses/ContentType/Readers/Derived/CharacterTemplateReader.cs
@@ -1,5 +1,6 @@
 ﻿using MagickaPUP.MagickaClasses.Character;
 using MagickaPUP.Utility.IO;
+using MagickaPUP.Utility.IO.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,41 @@ namespace MagickaPUP.XnaClasses.Specific.Derived
         { }
 
         public override CharacterTemplate Read(CharacterTemplate instance, MBinaryReader reader, DebugLogger logger = null)
+        {
+            CharacterTemplate ans = null;
+            switch (reader.GameVersion
+            {
+                default:
+                case GameVersion.Auto: // NOTE : This may or may not be a terrible fucking idea, but I could not come up with anything better after thinking a lot about it...
+                    try
+                    {
+                        ans = ReadNew(instance, reader, logger);
+                    }
+                    catch
+                    {
+                        ans = ReadOld(instance, reader, logger);
+                    }
+                    break; // If all else fails, then the input data is malformed as fuck!
+
+                case GameVersion.Old:
+                    ans = ReadOld(instance, reader, logger);
+                    break;
+
+                case GameVersion.New:
+                    ans = ReadNew(instance, reader, logger);
+                    break;
+            }
+            return ans;
+        }
+
+        private CharacterTemplate ReadOld(CharacterTemplate instance, MBinaryReader reader, DebugLogger logger = null)
+        {
+            CharacterTemplate ans = new CharacterTemplate();
+            ans.ReadInstance(reader, logger);
+            return ans;
+        }
+
+        private CharacterTemplate ReadNew(CharacterTemplate instance, MBinaryReader reader, DebugLogger logger = null)
         {
             CharacterTemplate ans = new CharacterTemplate();
             ans.ReadInstance(reader, logger);

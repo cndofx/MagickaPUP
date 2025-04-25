08e0f46b7736e05dbe2ff123be3d866ebf89727e
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-25 22:08:54 +0200

    Implemented EffectReader class

---
+++ MagickaPUP/MagickaPUP/XnaClasses/ContentType/Readers/Derived/EffectReader.cs
@@ -0,0 +1,23 @@
+﻿using MagickaPUP.Utility.IO;
+using MagickaPUP.XnaClasses.Specific;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.XnaClasses.ContentType.Readers.Derived
+{
+    public class EffectReader : TypeReader<EffectCode>
+    {
+        public EffectReader()
+        { }
+
+        public override EffectCode Read(EffectCode instance, MBinaryReader reader, DebugLogger logger = null)
+        {
+            EffectCode ans = new EffectCode();
+            ans.ReadInstance(reader, logger);
+            return ans;
+        }
+    }
+}

25fb7d9cdabac86c095e495364525bd62263fb80
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-25 22:09:02 +0200

    Implemented EffectWriter class

---
+++ MagickaPUP/MagickaPUP/XnaClasses/ContentType/Writers/Derived/EffectWriter.cs
@@ -0,0 +1,20 @@
+﻿using MagickaPUP.Utility.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.XnaClasses.ContentType.Writers.Derived
+{
+    public class EffectWriter : TypeWriter<EffectCode>
+    {
+        public EffectWriter()
+        { }
+
+        public override void Write(EffectCode instance, MBinaryWriter writer, DebugLogger logger = null)
+        {
+            instance.WriteInstance(writer, logger);
+        }
+    }
+}

c266e7a89fcffffc512fdf0a47dfb39ff4855132
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-26 09:21:04 +0200

    Added explanatory comment to EffectMaterial

--- MagickaPUP/MagickaPUP/XnaClasses/EffectMaterial.cs
+++ MagickaPUP/MagickaPUP/XnaClasses/EffectMaterial.cs
@@ -7,6 +7,16 @@ using System.Threading.Tasks;
 
 namespace MagickaPUP.XnaClasses
 {
+    #region Comment - EffectMaterial
+
+    // NOTE : This class corresponds to the XNA EffectMaterial class.
+    // Basically, this is just a dict for a bunch of properties with type tags and default values.
+    // Allows constructing a custom set of values for a material instance for a given shader type through a dictionary.
+    // When XNA loads this type of asset, it generates an Effect of the shader XNB type specified as External Reference here,
+    // and then assigns each of the properties within the dict to the created effect if they exist within the effect's code
+    // and if they are of a type supported by XNA / D3D's shaders.
+
+    #endregion
     public class EffectMaterial : XnaObject
     {
         public EffectMaterial()

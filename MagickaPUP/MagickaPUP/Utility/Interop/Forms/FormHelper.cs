d5747a1107583c8e27ab1a3f262a109d4b400c8e
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-26 06:39:27 +0200

    Created FormHelper static class
    The purpose of this class to aid with creating and destroying aux helper dummy forms.
    
    The only reason this is being done is so that we can get a proper D3D9 device initialized, since D3D9 requires a valid window handle to be used for the device to be created and initialized.
    
    Note that if a null pointer is passed, the current active window is apparently used as the window pointer for the device creation, which could lead to some behaviour that is not correct in any way, so this is why it's best to avoid said say of doing things.

---
+++ MagickaPUP/MagickaPUP/Utility/Interop/Forms/FormHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MagickaPUP.Utility.Interop.Forms
+{
+    public static class FormHelper
+    {
+        public static Form Create()
+        {
+            var dummyForm = new Form
+            {
+                Width = 1,
+                Height = 1,
+                ShowInTaskbar = false,
+                FormBorderStyle = FormBorderStyle.None,
+                StartPosition = FormStartPosition.Manual,
+                Location = new System.Drawing.Point(-32000, -32000) // I HATE MICROSOFT!!! FUCK YOU!!!
+            };
+            dummyForm.Show();
+            return dummyForm;
+        }
+
+        public static void Release(Form dummyForm)
+        {
+            dummyForm.Close();
+        }
+    }
+}

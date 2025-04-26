d6fe210e052013cc4ceff59d86fff68bb2491ae0
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-04-26 06:37:27 +0200

    Created ComHelper class
    This class is responsible for handling COM helper operations, such as releasing COM objects created in an unsafe manner.
    
    TODO : Very correctness of this function.

---
+++ MagickaPUP/MagickaPUP/Utility/Interop/Com/ComHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.Utility.Interop.Com
+{
+    public static class ComHelper
+    {
+        public static unsafe void Release(IntPtr comPtr)
+        {
+            // NOTE : All of these are possible alternatives that do not seem to work well with D3D9, or maybe I'm doing something wrong, idk.
+            // I'm just keeping these calls here just in case any of them becomes useful in the future...
+            // Marshal.ReleaseComObject(ptr);
+            // Marshal.Release(ptr);
+            // Marshal.FinalReleaseComObject(ptr);
+
+            if (comPtr == IntPtr.Zero)
+                return;
+
+            IntPtr* vtable = *(IntPtr**)comPtr;
+            delegate* unmanaged[Stdcall]<IntPtr, uint> release = (delegate* unmanaged[Stdcall]<IntPtr, uint>)vtable[2];
+            release(comPtr);
+        }
+    }
+}

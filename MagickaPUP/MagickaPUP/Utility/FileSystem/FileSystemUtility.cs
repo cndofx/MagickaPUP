46d92d5d84a1b79416e50fea8296d49ca649775c
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-07-30 16:18:42 +0200

    Created FSUtil class and added method to get path without extensions

---
+++ MagickaPUP/MagickaPUP/Utility/FileSystem/FileSystemUtility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagickaPUP.Utility.FileSystem
+{
+    // NOTE : Add other functionalities here in the future.
+    // Remember stuff like the FileSystemUtility class from the install manager program (mcow-mm).
+    public static class FSUtil
+    {
+        // Returns a cleaned up file path without extension
+        public static string GetPathWithoutExtension(string name)
+        {
+            string nameBase = Path.GetFileNameWithoutExtension(name);
+            string pathBase = Path.GetDirectoryName(name);
+
+            name = Path.Combine(pathBase, nameBase);
+
+            return name;
+        }
+    }
+}
